ac0cc1591db10e9af6d35df19915fb93b07e2b31
HelloKitty <andrew.blakely@ymail.com>
2018-04-01 14:27:51 -0500

    Added Optional attribute to AuthLogonChallengeResponse
    This allows responses to be sent from a server emulator containing just
    the result.

--- src/FreecraftCore.Packet.Auth/Payloads/Server/AuthLogonChallengeResponse.cs
+++ src/FreecraftCore.Packet.Auth/Payloads/Server/AuthLogonChallengeResponse.cs
@@ -4,6 +4,7 @@ using FreecraftCore.Packet.Common;
 using FreecraftCore.Serializer;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 
@@ -24,16 +25,23 @@ namespace FreecraftCore.Packet.Auth
 		/// Unknown 0x00 byte sent from the server.
 		/// </summary>
 		[WireMember(1)]
-		public readonly byte unknownResponseByte;
+		public readonly byte unknownResponseByte = 0;
 
 		//Second piece of data sent is a result
 		[WireMember(2)]
 		public AuthenticationResult Result { get; private set; }
 
 		/// <summary>
+		/// Bool used to compute optional read/write for response payload fields
+		/// that involved the SRP.
+		/// </summary>
+		public bool isAuthenticationChallengeSuccessful => Result == AuthenticationResult.Success || Result == AuthenticationResult.Success_Survey;
+
+		/// <summary>
 		/// The SRP6 challenge provided by the server.
 		/// See: http://srp.stanford.edu/design.html for more information
 		/// </summary>
+		[Optional(nameof(isAuthenticationChallengeSuccessful))]
 		[WireMember(3)]
 		public SRPToken Challenge { get; private set; }
 
@@ -41,6 +49,7 @@ namespace FreecraftCore.Packet.Auth
 		/// Seed to be used during HMAC of the client files.
 		/// (Not implemented in Trinitycore or Mangos)
 		/// </summary>
+		[Optional(nameof(isAuthenticationChallengeSuccessful))]
 		[KnownSize(16)]
 		[WireMember(4)]
 		public byte[] ClientFileHMACSeed { get; private set; }
@@ -50,6 +59,7 @@ namespace FreecraftCore.Packet.Auth
 		/// Flags that indicate what security additional measures are required for authentication
 		/// (Ex. Authenticator or Pin) Will usually be 0 meaning none
 		/// </summary>
+		[Optional(nameof(isAuthenticationChallengeSuccessful))]
 		[WireMember(5)]
 		public byte securityFlags { get; private set; }
 
@@ -58,9 +68,20 @@ namespace FreecraftCore.Packet.Auth
 		//It's all just 0s or constants anyway in Trinitycore
 
 		//If this is true then it will contain token data
+		[Obsolete("Use success property.")]
 		public bool isSuccess()
 		{
 			return Result == AuthenticationResult.Success || Result == AuthenticationResult.Success_Survey;
+		}
+
+		public AuthLogonChallengeResponse(AuthenticationResult result)
+		{
+			if(!Enum.IsDefined(typeof(AuthenticationResult), result)) throw new InvalidEnumArgumentException(nameof(result), (int)result, typeof(AuthenticationResult));
+
+			Result = result;
+
+			if(isAuthenticationChallengeSuccessful)
+				throw new InvalidOperationException($"Cannot initialize {nameof(Result)} as {AuthenticationResult.Success} without fully intializing payload.");
 		}
 
 		//TODO: Proper ctor; right now we don't have a server so we can get away with just default
