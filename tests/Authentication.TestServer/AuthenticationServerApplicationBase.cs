0bc97d4ae637a8f80f537cbdd15f2fe35f4feec6
HelloKitty <andrew.blakely@ymail.com>
2018-04-01 14:28:06 -0500

    Added GladNet3 server emulator demo for AuthServer

---
+++ tests/Authentication.TestServer/AuthenticationServerApplicationBase.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Common.Logging;
+using Common.Logging.Simple;
+using FreecraftCore;
+using FreecraftCore.API.Common;
+using FreecraftCore.Packet.Auth;
+using FreecraftCore.Serializer;
+using GladNet;
+using GladNet3;
+
+namespace Authentication.TestServer
+{
+	public sealed class AuthenticationServerApplicationBase : TcpServerServerApplicationBase<AuthenticationServerPayload, AuthenticationClientPayload>
+	{
+		private INetworkSerializationService Serializer { get; }
+
+		/// <inheritdoc />
+		public AuthenticationServerApplicationBase(NetworkAddressInfo serverAddress) 
+			: base(serverAddress)
+		{
+			Serializer = new FreecraftCoreGladNetSerializerAdapter(CreateSerializer());
+		}
+
+		private ISerializerService CreateSerializer()
+		{
+			SerializerService serializer = new SerializerService();
+
+			typeof(AuthLogonChallengeRequest).Assembly
+				.GetTypes()
+				.Where(t => typeof(AuthenticationServerPayload).IsAssignableFrom(t) || typeof(AuthenticationClientPayload).IsAssignableFrom(t))
+				.ToList()
+				.ForEach(t =>
+				{
+					serializer.RegisterType(t);
+				});
+
+			serializer.Compile();
+
+			return serializer;
+		}
+
+		/// <inheritdoc />
+		protected override async Task HandleIncomingNetworkMessage(IManagedNetworkClient<AuthenticationServerPayload, AuthenticationClientPayload> networkClient, NetworkIncomingMessage<AuthenticationClientPayload> message)
+		{
+			Console.WriteLine($"Recieved Payload: {message.Payload.GetType().Name}");
+
+			//This is just demo code
+			if(message.Payload is AuthLogonChallengeRequest c)
+				Console.WriteLine($"ClientBuild: {c.Build.ToString()}");
+
+			AuthLogonChallengeResponse response = new AuthLogonChallengeResponse(AuthenticationResult.FailNoGameTime);
+
+			await networkClient.SendMessage(response);
+
+			//await networkClient.DisconnectAsync(1);
+		}
+
+		/// <inheritdoc />
+		protected override bool IsClientAcceptable(TcpClient tcpClient)
+		{
+			return true;
+		}
+
+		/// <inheritdoc />
+		protected override IManagedNetworkClient<AuthenticationServerPayload, AuthenticationClientPayload> CreateIncomingClientPipeline(TcpClient client)
+		{
+			Console.WriteLine($"Client Connected: {(client.Client.RemoteEndPoint as IPEndPoint)?.Address.ToString()}");
+
+			//The auth server is encryptionless and headerless
+			IManagedNetworkClient<AuthenticationServerPayload, AuthenticationClientPayload> managedClient = new DotNetTcpClientNetworkClient(client)
+				.AddHeaderlessNetworkMessageReading(Serializer)
+				.For<AuthenticationClientPayload, AuthenticationServerPayload, IAuthenticationPayload>()
+				.Build()
+				.AsManaged(new ConsoleOutLogger("ConsoleLogger", LogLevel.All, true, false, false, null));
+
+			return managedClient;
+		}
+	}
+}
---
+++ tests/Authentication.TestServer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using FreecraftCore.Packet.Auth;
+using GladNet;
+using GladNet3;
+
+namespace Authentication.TestServer
+{
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			NetworkAddressInfo info = new NetworkAddressInfo(IPAddress.Parse("127.0.0.1"), 3724);
+
+			AuthenticationServerApplicationBase authServer = new AuthenticationServerApplicationBase(info);
+
+			if(authServer.StartServer())
+				await authServer.BeginListening();
+			else
+			{
+				throw new InvalidOperationException($"Unable start server.");
+			}
+		}
+	}
+}
