8b72980396b0276ebfc821a84b79fda19b03cc29
HelloKitty <andrew.blakely@ymail.com>
2018-05-26 20:32:57 -0500

    Added AddonInfoEvent and ClientCacheVersionEvent

---
+++ src/FreecraftCore.Packet.Game/GamePacketMetadataMarker.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using FreecraftCore.Packet;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Type marker for the game packet assembly.
+	/// </summary>
+	public static class GamePacketMetadataMarker
+	{
+		public static IReadOnlyCollection<Type> GamePacketPayloadTypes { get; }
+
+		public static IReadOnlyCollection<Type> SerializableTypes { get; }
+
+		public static IReadOnlyCollection<Type> AssemblyTypes { get; }
+
+		static GamePacketMetadataMarker()
+		{
+			AssemblyTypes = typeof(GamePacketMetadataMarker)
+				.Assembly
+				.GetExportedTypes();
+
+			SerializableTypes = GamePacketPayloadTypes
+				.Concat(AssemblyTypes.Where(t => t.GetCustomAttribute<WireDataContractAttribute>() != null))
+				.Distinct()
+				.ToArray();
+
+			GamePacketPayloadTypes = AssemblyTypes
+				.Where(t => typeof(GamePacketPayload).IsAssignableFrom(t))
+				.ToArray();
+		}
+	}
+}
---
+++ src/FreecraftCore.Packet.Game/Packets/Setup/ClientCacheVersionEvent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Packet;
+using FreecraftCore.Packet.Common;
+using FreecraftCore.Serializer;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Sent to the connecting user before the character list.
+	/// Sent with <see cref="NetworkOperationCode.SMSG_CLIENTCACHE_VERSION"/>.
+	/// </summary>
+	[GamePayloadOperationCode(NetworkOperationCode.SMSG_CLIENTCACHE_VERSION)]
+	public sealed class ClientCacheVersionEvent : GamePacketPayload
+	{
+		/// <inheritdoc />
+		public override bool isValid => true;
+
+		[ReadToEnd]
+		[WireMember(1)]
+		public byte[] Data { get; }
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		protected ClientCacheVersionEvent()
+		{
+			
+		}
+	}
+}
