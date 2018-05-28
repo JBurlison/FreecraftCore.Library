86dac382ff16ab2a33c742a1ff42bb6cb01800bc
HelloKitty <andrew.blakely@ymail.com>
2018-05-28 13:16:44 -0500

    Added PingRequest temp DTO

---
+++ src/FreecraftCore.Packet.Game/Packets/PingRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+
+namespace FreecraftCore
+{
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.CMSG_PING)]
+	public sealed class PingRequest : GamePacketPayload
+	{
+		/// <inheritdoc />
+		public override bool isValid => true;
+
+		[ReadToEnd]
+		[WireMember(1)]
+		public byte[] Data { get; }
+
+		public PingRequest()
+		{
+			
+		}
+	}
+}
