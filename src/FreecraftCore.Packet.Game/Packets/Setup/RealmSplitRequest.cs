aacf1ea1eeeacd00f2c17e7da9d1d70cd835f942
HelloKitty <andrew.blakely@ymail.com>
2018-05-28 13:17:36 -0500

    ADDED CMSG_REALM_SPLIT and SMSG temp DTOs

---
+++ src/FreecraftCore.Packet.Game/Packets/Setup/RealmSplitRequest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Sent by the connecting user during character list.
+	/// Sent with <see cref="NetworkOperationCode.CMSG_REALM_SPLIT"/>.
+	/// </summary>
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.CMSG_REALM_SPLIT)]
+	public sealed class RealmSplitRequest : GamePacketPayload
+	{
+		/// <inheritdoc />
+		public override bool isValid => true;
+
+		[ReadToEnd]
+		[WireMember(1)]
+		public byte[] Data { get; }
+
+		public RealmSplitRequest()
+		{
+			
+		}
+	}
+}
---
+++ src/FreecraftCore.Packet.Game/Packets/Setup/RealmSplitResponse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Sent to the connecting during character list
+	/// in response to <see cref="NetworkOperationCode.CMSG_REALM_SPLIT"/>.
+	/// Sent with <see cref="NetworkOperationCode.SMSG_REALM_SPLIT"/>.
+	/// </summary>
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.SMSG_REALM_SPLIT)]
+	public sealed class RealmSplitResponse : GamePacketPayload
+	{
+		/// <inheritdoc />
+		public override bool isValid => true;
+
+		[ReadToEnd]
+		[WireMember(1)]
+		public byte[] Data { get; }
+
+		public RealmSplitResponse()
+		{
+			
+		}
+	}
+}
