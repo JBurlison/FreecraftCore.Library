5e442f82fb555468533b165e5fb78e2ed923d202
HelloKitty <andrew.blakely@ymail.com>
2018-05-28 13:16:56 -0500

    Added CMSG_READ_FOR_ACCOUNT_DATA_TIMES temp DTO

---
+++ src/FreecraftCore.Packet.Game/Packets/Setup/AccountDataTimesRequest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Sent by the connecting user during the character list.
+	/// Sent with <see cref="NetworkOperationCode.CMSG_READY_FOR_ACCOUNT_DATA_TIMES"/>.
+	/// </summary>
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.CMSG_READY_FOR_ACCOUNT_DATA_TIMES)]
+	public sealed class AccountDataTimesRequest : GamePacketPayload
+	{
+		/// <inheritdoc />
+		public override bool isValid => true;
+
+		[ReadToEnd]
+		[WireMember(1)]
+		public byte[] Data { get; }
+
+		public AccountDataTimesRequest()
+		{
+			
+		}
+	}
+}
