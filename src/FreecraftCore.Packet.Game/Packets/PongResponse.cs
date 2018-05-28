3747a1bc437bb16e517d6d7b720f65f5efb7f85c
HelloKitty <andrew.blakely@ymail.com>
2018-05-28 14:11:10 -0500

    Added PongResponse temp DTO

---
+++ src/FreecraftCore.Packet.Game/Packets/PongResponse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Response to a <see cref="NetworkOperationCode.CMSG_PING"/>.
+	/// </summary>
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.SMSG_PONG)]
+	public sealed class PongResponse : GamePacketPayload
+	{
+		/// <inheritdoc />
+		public override bool isValid => true;
+
+		[ReadToEnd]
+		[WireMember(1)]
+		public byte[] Data { get; }
+
+		public PongResponse()
+		{
+			
+		}
+	}
+}
