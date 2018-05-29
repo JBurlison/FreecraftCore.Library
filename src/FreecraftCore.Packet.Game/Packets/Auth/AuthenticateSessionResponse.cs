633c0be729e8ea4aa7284c01a3829a2aba44f560
HelloKitty <andrew.blakely@ymail.com>
2018-05-29 13:46:39 -0500

    Added doc to denote 1.12.1 and 3.3.5 SMSG_AUTH_RESPONSE are the same

--- src/FreecraftCore.Packet.Game/Packets/Auth/AuthenticateSessionResponse.cs
+++ src/FreecraftCore.Packet.Game/Packets/Auth/AuthenticateSessionResponse.cs
@@ -2,6 +2,10 @@
 
 namespace FreecraftCore
 {
+	/// <summary>
+	/// The authentication response to <see cref="NetworkOperationCode.CMSG_AUTH_SESSION"/>
+	/// Same for 1.12.1 and 3.3.5
+	/// </summary>
 	[WireDataContract]
 	[GamePayloadOperationCode(NetworkOperationCode.SMSG_AUTH_RESPONSE)] //the response to a request to authenticate a session
 	[ProtocolGrouping(ProtocolCode.Authentication)] //Though this isn't part of the actual authserver stuff it's still auth.
