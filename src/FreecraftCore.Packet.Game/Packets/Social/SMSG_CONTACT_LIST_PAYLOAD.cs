bcdc2631ad24e9ef32fb1ea69a1008ec422db9c2
HelloKitty <andrew.blakely@ymail.com>
2018-05-30 14:07:59 -0500

    Partially implement SMSG_CONTACT_LIST

---
+++ src/FreecraftCore.Packet.Game/Packets/Social/SMSG_CONTACT_LIST_PAYLOAD.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+
+namespace FreecraftCore
+{
+	//TODO: This is not fully implemented.
+	/// <summary>
+	/// Send during initial before joining map on
+	/// Trinitycore. Before player is joined into the world.
+	/// </summary>
+	[GamePayloadOperationCode(NetworkOperationCode.SMSG_CONTACT_LIST)]
+	[WireDataContract]
+    public sealed class SMSG_CONTACT_LIST_PAYLOAD : GamePacketPayload
+	{
+		//TODO: Extract
+		[WireDataContract]
+		public enum SocialFlag : int
+		{
+			SOCIAL_FLAG_FRIEND = 0x01,
+			SOCIAL_FLAG_IGNORED = 0x02,
+			SOCIAL_FLAG_MUTED = 0x04,                          // guessed
+			SOCIAL_FLAG_UNK = 0x08,                          // Unknown - does not appear to be RaF
+
+			SOCIAL_FLAG_ALL = SOCIAL_FLAG_FRIEND | SOCIAL_FLAG_IGNORED | SOCIAL_FLAG_MUTED
+		}
+
+		/// <summary>
+		/// TODO: Implement flags enumeration for social type
+		/// </summary>
+		[WireMember(1)]
+		public SocialFlag Flags { get; } = SocialFlag.SOCIAL_FLAG_ALL;
+
+		/// <summary>
+		/// TODO: Implement the actual friends data
+		/// </summary>
+		[ReadToEnd]
+		[WireMember(2)]
+		public byte[] Data { get; }
+
+		/// <summary>
+		/// Creates an empty friends list packet.
+		/// </summary>
+		public SMSG_CONTACT_LIST_PAYLOAD()
+		{
+			
+		}
+    }
+}
