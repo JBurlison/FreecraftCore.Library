5398b910681f799d66465828cd524ce0307eb6de
HelloKitty <andrew.blakely@ymail.com>
2018-05-30 11:39:03 -0500

    Added 3.3.5 CMSG/SMSG UPDATE_ACCOUNT_DATA

---
+++ src/FreecraftCore.Packet.Game/Packets/Setup/CMSG_UPDATE_ACCOUNT_DATA_PAYLOAD - Copy.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Sent by the server once the client data is set.
+	/// </summary>
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.SMSG_UPDATE_ACCOUNT_DATA_COMPLETE)]
+	public sealed class SMSG_UPDATE_ACCOUNT_DATA_COMPLETE_PAYLOAD : GamePacketPayload
+	{
+		//TODO: Extract enum
+		/// <summary>
+		/// The account data type that completed.
+		/// </summary>
+		[WireMember(1)]
+		public CMSG_UPDATE_ACCOUNT_DATA_PAYLOAD.AccountDataType DataType { get; }
+
+		//TODO: Investigate
+		/// <summary>
+		/// Unknown data? Maybe indicates success?
+		/// </summary>
+		[WireMember(2)]
+		private int Unk { get; } = 0;
+
+		/// <inheritdoc />
+		public SMSG_UPDATE_ACCOUNT_DATA_COMPLETE_PAYLOAD(CMSG_UPDATE_ACCOUNT_DATA_PAYLOAD.AccountDataType dataType)
+		{
+			DataType = dataType;
+		}
+
+		protected SMSG_UPDATE_ACCOUNT_DATA_COMPLETE_PAYLOAD()
+		{
+			
+		}
+	}
+}
---
+++ src/FreecraftCore.Packet.Game/Packets/Setup/CMSG_UPDATE_ACCOUNT_DATA_PAYLOAD.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Sent by the client to set some client data.
+	/// </summary>
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.CMSG_UPDATE_ACCOUNT_DATA)]
+	public sealed class CMSG_UPDATE_ACCOUNT_DATA_PAYLOAD : GamePacketPayload
+	{
+		//TODO: Extract out of the class
+		public enum AccountDataType : int
+		{
+			GLOBAL_CONFIG_CACHE = 0,                    // 0x01 g
+			PER_CHARACTER_CONFIG_CACHE = 1,                    // 0x02 p
+			GLOBAL_BINDINGS_CACHE = 2,                    // 0x04 g
+			PER_CHARACTER_BINDINGS_CACHE = 3,                    // 0x08 p
+			GLOBAL_MACROS_CACHE = 4,                    // 0x10 g
+			PER_CHARACTER_MACROS_CACHE = 5,                    // 0x20 p
+			PER_CHARACTER_LAYOUT_CACHE = 6,                    // 0x40 p
+			PER_CHARACTER_CHAT_CACHE = 7                     // 0x80 p
+		}
+
+		/// <summary>
+		/// The type of data.
+		/// </summary>
+		[WireMember(1)]
+		public AccountDataType DataType { get; }
+
+		//TODO: Create timestamp type.
+		/// <summary>
+		/// The time stamp? Maybe when the client created the data?
+		/// </summary>
+		[Encoding(EncodingType.ASCII)]
+		[WireMember(2)]
+		public string TimeStamp { get; }
+
+		//Some compressed data here
+		//On TC it appears to be a string. Might be planitext LUA? Config text?
+		/// <summary>
+		/// Text-based configuration data.
+		/// </summary>
+		[Compress]
+		[Encoding(EncodingType.ASCII)]
+		[WireMember(3)]
+		public string ConfigData { get; }
+
+		/// <inheritdoc />
+		public CMSG_UPDATE_ACCOUNT_DATA_PAYLOAD(AccountDataType dataType, [NotNull] string timeStamp, [NotNull] string configData)
+		{
+			DataType = dataType;
+			TimeStamp = timeStamp ?? throw new ArgumentNullException(nameof(timeStamp));
+			ConfigData = configData ?? throw new ArgumentNullException(nameof(configData));
+		}
+
+
+		protected CMSG_UPDATE_ACCOUNT_DATA_PAYLOAD()
+		{
+			
+		}
+	}
+}
