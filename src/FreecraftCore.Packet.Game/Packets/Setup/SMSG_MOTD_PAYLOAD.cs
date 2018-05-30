4f1c5fc2a90100f47190c57d42c8e5b8d9126864
HelloKitty <andrew.blakely@ymail.com>
2018-05-30 13:22:58 -0500

    Implemented wotlk SMSG_MOTD

---
+++ src/FreecraftCore.Packet.Game/Packets/Setup/SMSG_MOTD_PAYLOAD.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+
+namespace FreecraftCore
+{
+	//TC says new in 2.0.1 which leads me to believe 3.3.5 and 1.12.1 will have differing structures.
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.SMSG_MOTD)]
+	public sealed class SMSG_MOTD_PAYLOAD : GamePacketPayload
+	{
+		//the MOTD on Trinitycore is implemented as a tokenized string
+		//They split by the token and then it becomes a length-prefixed array of null terminated ASCII
+		//strings meaning we can easily handle that.
+		[Encoding(EncodingType.ASCII)]
+		[WireMember(1)]
+		[SendSize(SendSizeAttribute.SizeType.Int32)]
+		private string[] MessageOfTheDayByLines { get; }
+
+		/// <summary>
+		/// The message of the day split onto individual lines.
+		/// </summary>
+		public IReadOnlyCollection<string> MessageOfTheDay => MessageOfTheDayByLines;
+
+		/// <inheritdoc />
+		public SMSG_MOTD_PAYLOAD([NotNull] string[] messageOfTheDayByLines)
+		{
+			MessageOfTheDayByLines = messageOfTheDayByLines ?? throw new ArgumentNullException(nameof(messageOfTheDayByLines));
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		protected SMSG_MOTD_PAYLOAD()
+		{
+			
+		}
+	}
+}
