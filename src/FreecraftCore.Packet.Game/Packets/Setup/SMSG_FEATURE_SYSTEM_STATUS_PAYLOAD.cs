ad1738f10d2de80fc27875abb7273d4f65a3e793
HelloKitty <andrew.blakely@ymail.com>
2018-05-30 13:15:54 -0500

    Implemented SMSG_FEATURE_SYSTEM_STATUS

---
+++ src/FreecraftCore.Packet.Game/Packets/Setup/SMSG_FEATURE_SYSTEM_STATUS_PAYLOAD.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+
+namespace FreecraftCore
+{
+	//Not sent in 1.12.1. TC says this is about enabling features such as voice chat.
+	/// <summary>
+	/// Packet sent by the server during a session login.
+	/// It indicates a feature is enabled or disabled.
+	/// </summary>
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.SMSG_FEATURE_SYSTEM_STATUS)]
+	public sealed class SMSG_FEATURE_SYSTEM_STATUS_PAYLOAD : GamePacketPayload
+	{
+		//TODO: What is this?
+		/// <summary>
+		/// Unknown value. Maybe the feature type.
+		/// </summary>
+		[WireMember(1)]
+		public byte Unk { get; } = 2;
+
+		/// <summary>
+		/// Indicates if the feature is enabled.
+		/// </summary>
+		[WireMember(2)]
+		public bool isFeatureEnabled { get; }
+
+		/// <inheritdoc />
+		public SMSG_FEATURE_SYSTEM_STATUS_PAYLOAD(bool isFeatureEnabled)
+		{
+			this.isFeatureEnabled = isFeatureEnabled;
+		}
+
+		protected SMSG_FEATURE_SYSTEM_STATUS_PAYLOAD()
+		{
+			
+		}
+	}
+}
