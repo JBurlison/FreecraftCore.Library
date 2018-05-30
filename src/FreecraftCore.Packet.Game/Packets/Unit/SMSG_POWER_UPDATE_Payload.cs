65807d6e01a5fdf7046a4bfefab373c36a6a87b2
HelloKitty <andrew.blakely@ymail.com>
2018-05-30 16:49:59 -0500

    Implemented SMSG_POWER_UPDATE_Payload wotlk

---
+++ src/FreecraftCore.Packet.Game/Packets/Unit/SMSG_POWER_UPDATE_Payload.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Send by the server to update a unit's power field.
+	/// </summary>
+	[GamePayloadOperationCode(NetworkOperationCode.SMSG_POWER_UPDATE)]
+	[WireDataContract]
+	public sealed class SMSG_POWER_UPDATE_Payload : GamePacketPayload
+	{
+		//TODO: Extract
+		public enum Powers : byte
+		{
+			POWER_MANA = 0,
+			POWER_RAGE = 1,
+			POWER_FOCUS = 2,
+			POWER_ENERGY = 3,
+			POWER_HAPPINESS = 4,
+			POWER_RUNE = 5,
+			POWER_RUNIC_POWER = 6,
+			MAX_POWERS = 7,
+			POWER_ALL = 127,    // default for class?
+
+			//TODO: Sent as byte but why is this on TC?
+			//POWER_HEALTH = 0xFFFFFFFE    // (-2 as signed value)
+		};
+
+		/// <summary>
+		/// The GUID that had the power update.
+		/// </summary>
+		[WireMember(1)]
+		public PackedGuid UnitGuid { get; }
+
+		/// <summary>
+		/// The power type being updated.
+		/// </summary>
+		[WireMember(2)]
+		public Powers PowerType { get; }
+
+		/// <summary>
+		/// The new power value.
+		/// </summary>
+		[WireMember(3)]
+		public int NewValue { get; }
+
+		/// <inheritdoc />
+		public SMSG_POWER_UPDATE_Payload(PackedGuid unitGuid, Powers powerType, int newValue)
+		{
+			UnitGuid = unitGuid;
+			PowerType = powerType;
+			NewValue = newValue;
+		}
+
+		/// <summary>
+		/// Serializer ctor
+		/// </summary>
+		protected SMSG_POWER_UPDATE_Payload()
+		{
+			
+		}
+	}
+}
