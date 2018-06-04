f64ddada72e4d8018f8e09df4fdfb0019dde81ce
HelloKitty <andrew.blakely@ymail.com>
2018-06-04 18:32:56 -0500

    Implemented SMSG_WORLD_STATE_UI_TIMER_UPDATE for 3.3.5
    1.12.1 doesn't have it I don't think, maybe a similar packet?

---
+++ src/FreecraftCore.Packet.Game/Packets/Setup/SMSG_WORLD_STATE_UI_TIMER_UPDATE_Payload.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+
+namespace FreecraftCore
+{
+	//1.12.1 doesn't have this opcode.
+	/// <summary>
+	/// Sent to the server in an attempt to syncronize time from the 3.3.5 client.
+	/// </summary>
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.SMSG_WORLD_STATE_UI_TIMER_UPDATE)]
+	public sealed class SMSG_WORLD_STATE_UI_TIMER_UPDATE_Payload : GamePacketPayload
+	{
+		/// <summary>
+		/// The current time.
+		/// </summary>
+		[WireMember(1)]
+		public uint CurrentTime { get; }
+
+		/// <inheritdoc />
+		public SMSG_WORLD_STATE_UI_TIMER_UPDATE_Payload(uint currentTime)
+		{
+			CurrentTime = currentTime;
+		}
+
+		/// <summary>
+		/// Default creation initializes the time to current time.
+		/// </summary>
+		public SMSG_WORLD_STATE_UI_TIMER_UPDATE_Payload()
+		{
+			
+		}
+	}
+}
