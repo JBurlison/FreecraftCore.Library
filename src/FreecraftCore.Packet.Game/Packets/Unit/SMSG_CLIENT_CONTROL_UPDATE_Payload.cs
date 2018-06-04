b7cff6591f3e0181cc852d9733770e9d92f204e9
HelloKitty <andrew.blakely@ymail.com>
2018-06-04 18:23:34 -0500

    Implemented SMSG_CLIENT_CONTROL_UPDATE
    Same for 3.3.5 and 1.12.1 I think.

---
+++ src/FreecraftCore.Packet.Game/Packets/Unit/SMSG_CLIENT_CONTROL_UPDATE_Payload.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Sent by the server when logging in, and in other cases too,
+	/// that should enable control of the guid.
+	/// </summary>
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.SMSG_CLIENT_CONTROL_UPDATE)]
+	public sealed class SMSG_CLIENT_CONTROL_UPDATE_Payload : GamePacketPayload
+	{
+		//Both 1.12.1 and 3.3.5 send a packed guid.
+		/// <summary>
+		/// The object to update control of.
+		/// </summary>
+		[WireMember(1)]
+		public PackedGuid ControlledObject { get; }
+
+		/// <summary>
+		/// Indicates if the object is enabled??? TODO: Maybe this means is controlled disbale/enable?
+		/// </summary>
+		[WireMember(2)]
+		public bool IsEnabled { get; }
+
+		/// <inheritdoc />
+		public SMSG_CLIENT_CONTROL_UPDATE_Payload([NotNull] PackedGuid controlledObject, bool isEnabled)
+		{
+			ControlledObject = controlledObject ?? throw new ArgumentNullException(nameof(controlledObject));
+			IsEnabled = isEnabled;
+		}
+
+		/// <summary>
+		/// Serializer
+		/// </summary>
+		protected SMSG_CLIENT_CONTROL_UPDATE_Payload()
+		{
+			
+		}
+	}
+}
