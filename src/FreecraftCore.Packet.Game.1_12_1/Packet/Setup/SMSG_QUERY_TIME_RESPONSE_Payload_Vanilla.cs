d56839d738593bc55e50b77cde37cbe005094060
HelloKitty <andrew.blakely@ymail.com>
2018-06-04 18:17:39 -0500

    Implemented SMSG_QUERY_TIME_RESPONSE for 1.12.1 and 3.3.5

---
+++ src/FreecraftCore.Packet.Game.1_12_1/Packet/Setup/SMSG_QUERY_TIME_RESPONSE_Payload_Vanilla.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Server response to <see cref="NetworkOperationCode.CMSG_QUERY_TIME"/>
+	/// for 1.12.1.
+	/// </summary>
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.SMSG_QUERY_TIME_RESPONSE)]
+	public class SMSG_QUERY_TIME_RESPONSE_Payload_Vanilla : GamePacketPayload
+	{
+		/// <summary>
+		/// The current time.
+		/// </summary>
+		[WireMember(1)]
+		public uint CurrentTime { get; }
+
+		//1.12.1 only sends current time. Has no daily
+
+		/// <inheritdoc />
+		public SMSG_QUERY_TIME_RESPONSE_Payload_Vanilla(uint currentTime)
+		{
+			CurrentTime = currentTime;
+		}
+
+		/// <summary>
+		/// Creates a default time response payload
+		/// with the current time.
+		/// </summary>
+		public SMSG_QUERY_TIME_RESPONSE_Payload_Vanilla()
+		{
+
+		}
+	}
+}
---
+++ src/FreecraftCore.Packet.Game/Packets/Setup/SMSG_QUERY_TIME_RESPONSE_Payload.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+
+namespace FreecraftCore
+{
+	//1.12.1 sends approximately the same packet but without daily quest time.
+	/// <summary>
+	/// Server response to <see cref="NetworkOperationCode.CMSG_QUERY_TIME"/>.
+	/// </summary>
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.SMSG_QUERY_TIME_RESPONSE)]
+	public class SMSG_QUERY_TIME_RESPONSE_Payload : GamePacketPayload
+	{
+		/// <summary>
+		/// The current time.
+		/// </summary>
+		[WireMember(1)]
+		public uint CurrentTime { get; }
+
+		/// <summary>
+		/// The time remaining between the current time and the
+		/// daily quest reset.
+		/// </summary>
+		[WireMember(2)]
+		public uint DailyQuestTime { get; }
+
+		/// <inheritdoc />
+		public SMSG_QUERY_TIME_RESPONSE_Payload(uint currentTime, uint dailyQuestTime)
+		{
+			CurrentTime = currentTime;
+			DailyQuestTime = dailyQuestTime;
+		}
+
+		/// <summary>
+		/// Creates a default time response payload
+		/// with the current time.
+		/// </summary>
+		public SMSG_QUERY_TIME_RESPONSE_Payload()
+		{
+			
+		}
+	}
+}
