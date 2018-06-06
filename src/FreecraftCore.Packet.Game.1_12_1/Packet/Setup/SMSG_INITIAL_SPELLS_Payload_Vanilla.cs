43919a9e30b5ad2368a2a957146bb1bfabbf2209
HelloKitty <andrew.blakely@ymail.com>
2018-06-06 16:01:03 -0500

    Implemented SMSG_INITIAL_SPELLS for 3.3.5 and 1.12.1

---
+++ src/FreecraftCore.Packet.Game.1_12_1/Packet/Setup/SMSG_INITIAL_SPELLS_Payload_Vanilla.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+
+namespace FreecraftCore
+{
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.SMSG_INITIAL_SPELLS)]
+	public sealed class SMSG_INITIAL_SPELLS_Payload_Vanilla : GamePacketPayload
+	{
+		/// <summary>
+		/// The initial spell data and cooldown data.
+		/// Uses int spell ids in 1.12.1.
+		/// </summary>
+		[WireMember(1)]
+		public InitialSpellDataBlock<ushort> Data { get; }
+
+		/// <inheritdoc />
+		public SMSG_INITIAL_SPELLS_Payload_Vanilla(InitialSpellDataBlock<ushort> data)
+		{
+			Data = data;
+		}
+
+		protected SMSG_INITIAL_SPELLS_Payload_Vanilla()
+		{
+
+		}
+	}
+}
---
+++ src/FreecraftCore.Packet.Game/Packets/Setup/SMSG_INITIAL_SPELLS_Payload.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+
+namespace FreecraftCore
+{
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.SMSG_INITIAL_SPELLS)]
+	public sealed class SMSG_INITIAL_SPELLS_Payload : GamePacketPayload
+	{
+		/// <summary>
+		/// The initial spell data and cooldown data.
+		/// Uses int spell ids in 3.3.5.
+		/// </summary>
+		[WireMember(1)]
+		public InitialSpellDataBlock<int> Data { get; }
+
+		/// <inheritdoc />
+		public SMSG_INITIAL_SPELLS_Payload(InitialSpellDataBlock<int> data)
+		{
+			Data = data;
+		}
+
+		protected SMSG_INITIAL_SPELLS_Payload()
+		{
+			
+		}
+	}
+}
