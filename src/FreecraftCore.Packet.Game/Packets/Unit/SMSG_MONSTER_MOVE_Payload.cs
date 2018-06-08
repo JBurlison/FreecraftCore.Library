15cac934fede27bf2fd7a15337dbfdf712e1de32
HelloKitty <andrew.blakely@ymail.com>
2018-06-08 18:57:15 -0500

    Refactored SMSG_MONSTER_MOVE for 3.3.5

--- src/FreecraftCore.Packet.Game/Packets/Unit/SMSG_MONSTER_MOVE_Payload.cs
+++ src/FreecraftCore.Packet.Game/Packets/Unit/SMSG_MONSTER_MOVE_Payload.cs
@@ -45,77 +45,30 @@ namespace FreecraftCore
 		public bool IsMovePacket => MoveInfo.MoveType != MonsterMoveType.MonsterMoveStop;
 
 		/// <summary>
-		/// Default value (or stop value) is <see cref="SplineMoveFlags.None"/>
+		/// Optional spline data sent if <see cref="IsMovePacket"/> is true.
+		/// Meaning the monster state isn't <see cref="MonsterMoveType.MonsterMoveStop"/>.
 		/// </summary>
 		[Optional(nameof(IsMovePacket))]
 		[WireMember(6)]
-		public SplineMoveFlags SplineFlags { get; } = SplineMoveFlags.None;
-
-		/// <summary>
-		/// Indicates if the move information has animation
-		/// data <see cref="OptionalAnimationInformation"/>
-		/// </summary>
-		public bool HasAnimationInformation => IsMovePacket && SplineFlags.HasFlag(SplineMoveFlags.Animation);
-
-		[Optional(nameof(HasAnimationInformation))]
-		[WireMember(7)]
-		public AnimationInfo OptionalAnimationInformation { get; }
-
-		/// <summary>
-		/// The duration of the spline.
-		/// </summary>
-		[Optional(nameof(IsMovePacket))]
-		[WireMember(8)]
-		public int SplineDuration { get; }
-
-		/// <summary>
-		/// Indicates if the move information has parabolic spline information.
-		/// </summary>
-		public bool HasParabolicSplineInfo => IsMovePacket && SplineFlags.HasFlag(SplineMoveFlags.Parabolic);
-
-		[Optional(nameof(HasParabolicSplineInfo))]
-		[WireMember(9)]
-		public ParabolicMoveInfo OptionalParabolicSplineInformation { get; }
-
-		public bool HasLinearPath => IsMovePacket && !HasCatMulRomSpline;
-
-		/// <summary>
-		/// Indicates if the optional cyclic catmulrom spline path information
-		/// is in the packet.
-		/// </summary>
-		public bool HasCatMulRomSpline => IsMovePacket && (SplineFlags & SplineMoveFlags.Mask_CatmullRom) != 0;
-
-		[SendSize(SendSizeAttribute.SizeType.Int32)] //we have to add 1 to the size, it's what TC does
-		[Optional(nameof(HasCatMulRomSpline))]
-		[WireMember(10)]
-		public Vector3<float>[] OptionalCatMulRomSplinePoints { get; }
-
-		[Optional(nameof(HasLinearPath))]
-		[WireMember(11)]
-		public LinearPathMoveInfo OptionalLinearPathInformation { get; }
+		public MonsterSplineInfo OptionalSplineInformation { get; }
 
 		//TODO: Additional ctors
 		//TODO: Validate parameters.
 		/// <inheritdoc />
-		public SMSG_MONSTER_MOVE_Payload(PackedGuid monsterGuid, byte unk1, Vector3<float> initialMovePoint, int splineId, MonsterMoveInfo moveInfo, SplineMoveFlags splineFlags, AnimationInfo optionalAnimationInformation, int splineDuration, ParabolicMoveInfo optionalParabolicSplineInformation, Vector3<float>[] optionalCatMulRomSplinePoints, LinearPathMoveInfo optionalLinearPathInformation)
+		public SMSG_MONSTER_MOVE_Payload(PackedGuid monsterGuid, byte unk1, Vector3<float> initialMovePoint, int splineId, MonsterMoveInfo moveInfo, MonsterSplineInfo optionalSplineInformation)
 		{
 			MonsterGuid = monsterGuid;
 			Unk1 = unk1;
 			InitialMovePoint = initialMovePoint;
 			SplineId = splineId;
 			MoveInfo = moveInfo;
-			SplineFlags = splineFlags;
-			OptionalAnimationInformation = optionalAnimationInformation;
-			SplineDuration = splineDuration;
-			OptionalParabolicSplineInformation = optionalParabolicSplineInformation;
-			OptionalCatMulRomSplinePoints = optionalCatMulRomSplinePoints;
-			OptionalLinearPathInformation = optionalLinearPathInformation;
+			OptionalSplineInformation = optionalSplineInformation;
 		}
 
 		/// <inheritdoc />
 		protected SMSG_MONSTER_MOVE_Payload()
 		{
-			
+
 		}
 	}
 }
