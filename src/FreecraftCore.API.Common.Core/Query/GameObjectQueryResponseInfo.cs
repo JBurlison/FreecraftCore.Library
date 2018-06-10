a27631bef43c68dd69b2c03b880735665bbdffed
HelloKitty <andrew.blakely@ymail.com>
2018-06-09 22:12:03 -0500

    Implemented SMSG_GAMEOBJECT_QUERY_RESPONSE for 3.3.5

---
+++ src/FreecraftCore.API.Common.Core/Query/GameObjectQueryResponseInfo.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+
+namespace FreecraftCore
+{
+	[WireDataContract]
+	public sealed class GameObjectQueryResponseInfo
+	{
+		//TODO: Get enum for this
+		[WireMember(1)]
+		public int GameObjectType { get; }
+
+		[WireMember(2)]
+		public int DisplayId { get; }
+
+		[Encoding(EncodingType.ASCII)]
+		[KnownSize(4)]
+		[WireMember(3)]
+		public string[] Names { get; }
+
+		[Encoding(EncodingType.ASCII)]
+		[WireMember(3)]
+		public string IconName { get; }
+
+		[Encoding(EncodingType.ASCII)]
+		[WireMember(4)]
+		public string CastBarCaption { get; }
+
+		[Encoding(EncodingType.ASCII)]
+		[WireMember(5)]
+		public string UnkString { get; }
+
+		[KnownSize(sizeof(int) * 24)]
+		public byte[] Data { get; }
+
+		[WireMember(6)]
+		public float Size { get; }
+
+		[KnownSize(6)]
+		[WireMember(7)]
+		public int[] QuestItemIds { get; }
+
+		/// <inheritdoc />
+		public GameObjectQueryResponseInfo(int gameObjectType, int displayId, string[] names, string iconName, string castBarCaption, string unkString, byte[] data, float size, int[] questItemIds)
+		{
+			GameObjectType = gameObjectType;
+			DisplayId = displayId;
+			Names = names;
+			IconName = iconName;
+			CastBarCaption = castBarCaption;
+			UnkString = unkString;
+			Data = data;
+			Size = size;
+			QuestItemIds = questItemIds;
+		}
+
+		protected GameObjectQueryResponseInfo()
+		{
+			
+		}
+	}
+}
