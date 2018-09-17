adaa90418e3848e25bbab2d5c87684412ee3f903
HelloKitty <andrew.blakely@ymail.com>
2018-09-17 03:51:32 -0500

    Added auth reconnect challenge DTO

---
+++ src/FreecraftCore.Packet.Auth/Payloads/Client/AuthReconnectChallengeRequest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Payload for reconnecting challenge event.
+	/// </summary>
+	[WireDataContract]
+	[AuthenticationClientPayload(AuthOperationCode.AUTH_RECONNECT_CHALLENGE)] //TODO: Figure out how to support linking with the limited information.
+	public sealed class AuthReconnectChallengeRequest : AuthenticationClientPayload
+	{
+		//TODO: Remove this crap
+		/// <inheritdoc />
+		public override bool isValid => true;
+
+		[WireMember(1)]
+		public AuthChallengeData ChallengeData { get; }
+
+		/// <inheritdoc />
+		public AuthReconnectChallengeRequest([NotNull] AuthChallengeData challengeData)
+		{
+			ChallengeData = challengeData ?? throw new ArgumentNullException(nameof(challengeData));
+		}
+
+		/// <summary>
+		/// Serializer ctor
+		/// </summary>
+		private AuthReconnectChallengeRequest()
+		{
+			
+		}
+	}
+}
