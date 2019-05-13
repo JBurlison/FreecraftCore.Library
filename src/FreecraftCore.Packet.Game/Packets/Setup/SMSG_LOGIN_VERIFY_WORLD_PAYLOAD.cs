a43f96727459c8f8b146ccfd0a2e809bb676565a
HelloKitty <andrew.blakely@ymail.com>
2019-05-13 01:48:50 -0500

    Added SMSG_LOGIN_VERIFY_WORLD DTO

---
+++ src/FreecraftCore.Packet.Game/Packets/Setup/SMSG_LOGIN_VERIFY_WORLD_PAYLOAD.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+
+namespace FreecraftCore
+{
+	/*
+	data << pCurrChar->GetMapId();
+	data << pCurrChar->GetPositionX();
+	data << pCurrChar->GetPositionY();
+	data << pCurrChar->GetPositionZ();
+	data << pCurrChar->GetOrientation();
+	*/
+	
+	/// <summary>
+	/// Packet sent on initial entry of a character to the world.
+	/// </summary>
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.SMSG_LOGIN_VERIFY_WORLD)]
+	public sealed class SMSG_LOGIN_VERIFY_WORLD_PAYLOAD : GamePacketPayload
+	{
+		/// <summary>
+		/// The Map Id to load the character into.
+		/// </summary>
+		[WireMember(1)]
+		public int MapId { get; private set; }
+
+		/// <summary>
+		/// The position of the character.
+		/// </summary>
+		[WireMember(2)]
+		public Vector3<float> Position { get; private set; }
+
+		/// <summary>
+		/// The orientation of the player.
+		/// </summary>
+		[WireMember(3)]
+		public float Orientation { get; private set; }
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		private SMSG_LOGIN_VERIFY_WORLD_PAYLOAD()
+		{
+			
+		}
+	}
+}
