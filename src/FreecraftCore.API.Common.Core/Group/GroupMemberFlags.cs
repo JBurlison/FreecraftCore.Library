11201deb23bcec7f8b1716c4f29bd8c9d95f04a3
HelloKitty <andrew.blakely@ymail.com>
2019-05-16 05:40:42 -0500

    Added GroupMemberFlags

---
+++ src/FreecraftCore.API.Common.Core/Group/GroupMemberFlags.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreecraftCore
+{
+	[Flags]
+	public enum GroupMemberFlags : byte
+	{
+		MEMBER_FLAG_ASSISTANT = 0x01,
+		MEMBER_FLAG_MAINTANK = 0x02,
+		MEMBER_FLAG_MAINASSIST = 0x04
+	}
+}
