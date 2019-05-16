cb3fcca62b374e752c24efd129a5a14270e9c2fe
HelloKitty <andrew.blakely@ymail.com>
2019-05-16 09:19:24 -0500

    Added PartyOperation enum

---
+++ src/FreecraftCore.API.Common.Core/Group/PartyOperation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreecraftCore
+{
+	public enum PartyOperation : int
+	{
+		PARTY_OP_INVITE = 0,
+		PARTY_OP_UNINVITE = 1,
+		PARTY_OP_LEAVE = 2,
+		PARTY_OP_SWAP = 4
+	}
+}
