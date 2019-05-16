ee366d1dde6969db3c33768ae265341b131ab064
HelloKitty <andrew.blakely@ymail.com>
2019-05-16 04:20:03 -0500

    Implemented CMSG_GROUP_INVITE

---
+++ src/FreecraftCore.Packet.Game/Packets/Group/ClientGroupInviteRequest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+
+namespace FreecraftCore
+{
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.CMSG_GROUP_INVITE)]
+	public sealed class ClientGroupInviteRequest : GamePacketPayload
+	{
+		//For some reason Blizzard ONLY supports the option to invite based on string
+		//I think that's silly
+		[WireMember(1)]
+		public string PlayerToInvite { get; private set; }
+
+		/// <inheritdoc />
+		public ClientGroupInviteRequest([NotNull] string playerToInvite)
+		{
+			if(string.IsNullOrWhiteSpace(playerToInvite)) throw new ArgumentException("Value cannot be null or whitespace.", nameof(playerToInvite));
+
+			PlayerToInvite = playerToInvite;
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		private ClientGroupInviteRequest()
+		{
+			
+		}
+	}
+}
