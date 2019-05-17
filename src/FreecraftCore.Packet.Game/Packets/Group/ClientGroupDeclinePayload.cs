514e01956a1c0406908914cacc2f864cd4a4e7e8
HelloKitty <andrew.blakely@ymail.com>
2019-05-17 14:44:01 -0500

    Implemented CMSG_GROUP_DECLINE packet

---
+++ src/FreecraftCore.Packet.Game/Packets/Group/ClientGroupDeclinePayload.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+
+namespace FreecraftCore
+{
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.CMSG_GROUP_DECLINE)]
+	public sealed class ClientGroupDeclinePayload : GamePacketPayload
+	{
+		/// <summary>
+		/// This just sends an empty group decline packet.
+		/// </summary>
+		public ClientGroupDeclinePayload()
+		{
+
+		}
+	}
+}
