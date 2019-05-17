96262da200ec5cf3323c1f6dc00132e67fdc7dd9
HelloKitty <andrew.blakely@ymail.com>
2019-05-17 14:46:52 -0500

    Partially Implement SMSG_TRADE_STATUS packet

---
+++ src/FreecraftCore.Packet.Game/Packets/Trade/ServerTradeStatusPayload.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using FreecraftCore.Serializer;
+
+namespace FreecraftCore
+{
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.SMSG_TRADE_STATUS)]
+	public sealed class ServerTradeStatusPayload : GamePacketPayload
+	{
+		[WireMember(1)]
+		public TradeStatus Status { get; }
+
+		/// <inheritdoc />
+		public ServerTradeStatusPayload(TradeStatus status)
+		{
+			if(!Enum.IsDefined(typeof(TradeStatus), status)) throw new InvalidEnumArgumentException(nameof(status), (int)status, typeof(TradeStatus));
+
+			Status = status;
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		private ServerTradeStatusPayload()
+		{
+
+		}
+	}
+}
