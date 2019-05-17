e4c94576c68ee7311247b91b3437ab8c71783547
HelloKitty <andrew.blakely@ymail.com>
2019-05-17 14:45:55 -0500

    Implemented CMSG_BEGIN_TRADE packet

---
+++ src/FreecraftCore.Packet.Game/Packets/Trade/ClientBeginTradePayload.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+
+namespace FreecraftCore
+{
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.CMSG_BEGIN_TRADE)]
+	public sealed class ClientBeginTradePayload : GamePacketPayload
+	{
+		/// <summary>
+		/// This packet is just empty?? TC handles no data from it.
+		/// </summary>
+		public ClientBeginTradePayload()
+		{
+
+		}
+	}
+}
