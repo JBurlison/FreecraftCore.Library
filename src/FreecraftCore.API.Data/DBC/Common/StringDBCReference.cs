0933cc55b9f6d0de75ebfc9f8a667fd30da576e4
HelloKitty <andrew.blakely@ymail.com>
2020-04-06 03:46:32 -0500

    Added Common DBC structure from DBC Tools

---
+++ src/FreecraftCore.API.Data/DBC/Common/StringDBCReference.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+
+namespace FreecraftCore
+{
+	[WireDataContract]
+	public sealed class StringDBCReference
+	{
+		[WireMember(1)]
+		public uint StringReferenceOffset { get; private set; }
+
+		/// <inheritdoc />
+		public StringDBCReference(uint stringReferenceOffset)
+		{
+			StringReferenceOffset = stringReferenceOffset;
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		protected StringDBCReference()
+		{
+			
+		}
+	}
+}
