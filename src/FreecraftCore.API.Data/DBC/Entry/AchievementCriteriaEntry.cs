55e15820dd480d7458457a5661c2cfcb3cf817b7
HelloKitty <andrew.blakely@ymail.com>
2020-04-07 12:40:50 -0500

    Finished AchievementCriteria dbc

--- src/FreecraftCore.API.Data/DBC/Entry/AchievementCriteriaEntry.cs
+++ src/FreecraftCore.API.Data/DBC/Entry/AchievementCriteriaEntry.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
@@ -47,6 +48,46 @@ namespace FreecraftCore
 
 		[WireMember(5)]
 		public int Amount { get; private set; }
+
+		[WireMember(6)]
+		public AchievementCriteriaConditionDefinition Start { get; private set; }
+
+		[WireMember(7)]
+		public AchievementCriteriaConditionDefinition Fail { get; private set; }
+
+		[WireMember(8)]
+		public LocalizedStringDBC<TStringType> Description { get; private set; }
+
+		[WireMember(9)]
+		public AchievementFlags Flags { get; private set; }
+
+		[WireMember(10)]
+		public AchievementCriteriaConditionDefinition Timed { get; private set; }
+
+		[WireMember(11)]
+		public int Time { get; private set; }
+
+		[WireMember(12)]
+		public int UIOrder { get; private set; }
+
+		public AchievementCriteriaEntry(int achievementCriteriaId, int referredAchievementId, [NotNull] AchievementEntry<TStringType> achievementEntry, AchievementCriteriaTypes type, int assetId, int amount, [NotNull] AchievementCriteriaConditionDefinition start, [NotNull] AchievementCriteriaConditionDefinition fail, [NotNull] LocalizedStringDBC<TStringType> description, AchievementFlags flags, [NotNull] AchievementCriteriaConditionDefinition timed, int time, int uiOrder)
+		{
+			if (!Enum.IsDefined(typeof(AchievementFlags), flags)) throw new InvalidEnumArgumentException(nameof(flags), (int) flags, typeof(AchievementFlags));
+
+			AchievementCriteriaId = achievementCriteriaId;
+			ReferredAchievementId = referredAchievementId;
+			AchievementEntry = achievementEntry ?? throw new ArgumentNullException(nameof(achievementEntry));
+			Type = type;
+			AssetId = assetId;
+			Amount = amount;
+			Start = start ?? throw new ArgumentNullException(nameof(start));
+			Fail = fail ?? throw new ArgumentNullException(nameof(fail));
+			Description = description ?? throw new ArgumentNullException(nameof(description));
+			Flags = flags;
+			Timed = timed ?? throw new ArgumentNullException(nameof(timed));
+			Time = time;
+			UIOrder = uiOrder;
+		}
 
 		//DBC Tools requires this to be public.
 		/// <summary>
