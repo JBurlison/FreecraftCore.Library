f3e680cf4ee752969b6319bc27bfb51c47b4dcf4
HelloKitty <andrew.blakely@ymail.com>
2020-04-06 23:49:25 -0500

    Added MapTypes

---
+++ src/FreecraftCore.API.Data/Core/Map/MapTypes.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Lua_IsInInstance
+	/// </summary>
+	public enum MapTypes
+	{
+		/// <summary>
+		/// none
+		/// </summary>
+		COMMON = 0,
+
+		/// <summary>
+		/// party
+		/// </summary>
+		INSTANCE = 1,
+
+		/// <summary>
+		/// raid
+		/// </summary>
+		RAID = 2,
+
+		/// <summary>
+		/// pvp
+		/// </summary>
+		BATTLEGROUND = 3,
+
+		/// <summary>
+		/// arena
+		/// </summary>
+		ARENA = 4 
+	}
+}
