33e4bc70e6601fa4113505e51ad80d505d3ddca3
HelloKitty <andrew.blakely@ymail.com>
2020-04-07 09:42:53 -0500

    Added AchievementFaction

---
+++ src/FreecraftCore.API.Data/Core/Achievement/AchievementFaction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreecraftCore
+{
+	public enum AchievementFaction : int
+	{
+		HORDE = 0,
+		ALLIANCE = 1,
+		ANY = -1
+	};
+}
