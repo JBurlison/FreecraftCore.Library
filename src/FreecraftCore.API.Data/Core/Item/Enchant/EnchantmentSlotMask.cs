78e50e5341a8f33c1ea250a762382a4eb7bc4cfa
HelloKitty <andrew.blakely@ymail.com>
2020-04-08 13:29:36 -0500

    Added EnchantmentSlotMask

---
+++ src/FreecraftCore.API.Data/Core/Item/Enchant/EnchantmentSlotMask.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreecraftCore
+{
+	public enum EnchantmentSlotMask
+	{
+		CAN_SOULBOUND = 0x01,
+		UNK1 = 0x02,
+		UNK2 = 0x04,
+		UNK3 = 0x08
+	};
+}
