34d6c0fa2cffe5444616770a6b7297e5f07d577b
HelloKitty <andrew.blakely@ymail.com>
2020-04-08 13:53:30 -0500

    Added ItemEnchantmentConstants

---
+++ src/FreecraftCore.API.Data/Core/Item/Enchant/ItemEnchantmentConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreecraftCore
+{
+	public static class ItemEnchantmentConstants
+	{
+		//#define MAX_ITEM_ENCHANTMENT_EFFECTS 3
+		public const int MAX_ITEM_ENCHANTMENT_EFFECTS = 3;
+	}
+}
