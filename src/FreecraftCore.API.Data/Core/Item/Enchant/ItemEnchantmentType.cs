bd1fe7bc568be242c3996cc48df1dc28017a1cf3
HelloKitty <andrew.blakely@ymail.com>
2020-04-08 13:31:07 -0500

    Added ItemEnchantmentType

---
+++ src/FreecraftCore.API.Data/Core/Item/Enchant/ItemEnchantmentType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreecraftCore
+{
+	public enum ItemEnchantmentType
+	{
+		NONE = 0,
+		COMBAT_SPELL = 1,
+		DAMAGE = 2,
+		EQUIP_SPELL = 3,
+		RESISTANCE = 4,
+		STAT = 5,
+		TOTEM = 6,
+		USE_SPELL = 7,
+		PRISMATIC_SOCKET = 8
+	};
+}
