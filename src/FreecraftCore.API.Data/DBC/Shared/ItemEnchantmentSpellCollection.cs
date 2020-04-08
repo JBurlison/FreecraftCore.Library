8c8536decbdd4a111dd7f06611b4c3f8dbf3403c
HelloKitty <andrew.blakely@ymail.com>
2020-04-08 13:53:45 -0500

    Added ItemEnchantmentSpellCollection

---
+++ src/FreecraftCore.API.Data/DBC/Shared/ItemEnchantmentSpellCollection.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace FreecraftCore
+{
+	[Owned]
+	[WireDataContract]
+	public sealed class ItemEnchantmentSpellCollection
+	{
+		[Column("Type")]
+		[WireMember(1)]
+		public GenericStaticallySizedArrayChunkThree<ItemEnchantmentType> Types { get; private set; }
+
+		[Column("Amount")]
+		[WireMember(2)]
+		public GenericStaticallySizedArrayChunkThree<ItemEnchantmentType> Amounts { get; private set; }
+
+		[Column("SpellId")]
+		[WireMember(3)]
+		public GenericStaticallySizedArrayChunkThree<int> SpellIds { get; private set; }
+
+		public ItemEnchantmentSpellCollection([NotNull] GenericStaticallySizedArrayChunkThree<ItemEnchantmentType> types, [NotNull] GenericStaticallySizedArrayChunkThree<ItemEnchantmentType> amounts, [NotNull] GenericStaticallySizedArrayChunkThree<int> spellIds)
+		{
+			Types = types ?? throw new ArgumentNullException(nameof(types));
+			Amounts = amounts ?? throw new ArgumentNullException(nameof(amounts));
+			SpellIds = spellIds ?? throw new ArgumentNullException(nameof(spellIds));
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		public ItemEnchantmentSpellCollection()
+		{
+			
+		}
+	}
+}
