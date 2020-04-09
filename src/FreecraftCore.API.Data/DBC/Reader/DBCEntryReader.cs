7a3e20c76ef079169557f2181870cb8167bc6905
HelloKitty <andrew.blakely@ymail.com>
2020-04-09 06:12:23 -0500

    Added DBC Reader from DBCTools

---
+++ src/FreecraftCore.API.Data/DBC/Reader/DBCEntryReader.cs
@@ -0,0 +1,84 @@
+﻿using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace FreecraftCore
+{
+	public static class DBCEntryReader
+	{
+		public static IEnumerable<Type> RequiredSerializeableTypes => new Type[2] { typeof(DBCHeader), typeof(StringDBC) };
+	}
+
+	/// <summary>
+	/// DBC file/stream reader.
+	/// Providing ability to read/parse a DBC file from a stream.
+	/// </summary>
+	/// <typeparam name="TDBCEntryType">The entry type.</typeparam>
+	public sealed class DBCEntryReader<TDBCEntryType> : DbcReaderBase, IDbcEntryReader<TDBCEntryType>
+		where TDBCEntryType : IDBCEntryIdentifiable
+	{
+		/// <summary>
+		/// The serializer
+		/// </summary>
+		private ISerializerService Serializer { get; }
+
+		/// <inheritdoc />
+		public DBCEntryReader([NotNull] Stream dbcStream, [NotNull] ISerializerService serializer) 
+			: base(dbcStream)
+		{
+			Serializer = serializer ?? throw new ArgumentNullException(nameof(serializer));
+		}
+
+		public async Task<ParsedDBCFile<TDBCEntryType>> Parse()
+		{
+			DBCHeader header = null;
+			try
+			{
+				header = await ReadDBCHeader(Serializer);
+			}
+			catch(Exception e)
+			{
+				throw new InvalidOperationException($"Failed to read {nameof(DBCHeader)} for Type: {typeof(TDBCEntryType).Name}. Exception: {e.Message} \n\n Stack: {e.StackTrace}", e);
+			}
+
+			//The below is from the: https://github.com/TrinityCore/SpellWork/blob/master/SpellWork/DBC/DBCReader.cs
+			if(!header.IsDBC)
+				throw new InvalidOperationException($"Failed to load DBC for DBC Type: {typeof(TDBCEntryType)} Signature: {header.Signature}");
+
+			//TODO: Implement DBC string reading
+			return new ParsedDBCFile<TDBCEntryType>(await ReadDBCEntryBlock(header).ConfigureAwait(false));
+		}
+
+		private async Task<Dictionary<int, TDBCEntryType>> ReadDBCEntryBlock(DBCHeader header)
+		{
+			//Guessing the size here, no way to know.
+			Dictionary<int, TDBCEntryType> entryMap = new Dictionary<int, TDBCEntryType>(header.RecordsCount);
+
+			byte[] bytes = new byte[header.RecordSize * header.RecordsCount];
+
+			//Lock for safety, we don't want anyone else accessing the stream while we read it.
+			await ReadBytesIntoArrayFromStream(bytes);
+
+			DefaultStreamReaderStrategy reader = new DefaultStreamReaderStrategy(bytes);
+
+			for(int i = 0; i < header.RecordsCount; i++)
+			{
+				try
+				{
+					TDBCEntryType entry = Serializer.Deserialize<TDBCEntryType>(reader);
+					entryMap.Add((int)entry.EntryId, entry);
+				}
+				catch(Exception e)
+				{
+					throw new InvalidOperationException($"Encountered error reading entry Type: {typeof(TDBCEntryType).Name} at Entry count: {i} Exception: {e.Message} \n\n Stack: {e.StackTrace}", e);
+				}
+			}
+
+			return entryMap;
+		}
+	}
+}
---
+++ src/FreecraftCore.API.Data/DBC/Reader/DbcStringReader.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+
+namespace FreecraftCore
+{
+	public sealed class DbcStringReader : DbcReaderBase, IDbcStringReadable
+	{
+		/// <summary>
+		/// The serializer
+		/// </summary>
+		private ISerializerService Serializer { get; }
+
+		/// <inheritdoc />
+		public DbcStringReader([NotNull] Stream dbcStream, [NotNull] ISerializerService serializer) 
+			: base(dbcStream)
+		{
+			Serializer = serializer ?? throw new ArgumentNullException(nameof(serializer));
+		}
+
+		/// <inheritdoc />
+		public async Task<IReadOnlyDictionary<uint, string>> ParseOnlyStrings()
+		{
+			return await ReadDBCStringBlock(await this.ReadDBCHeader(Serializer));
+		}
+
+		//TODO: Does this work for 0 length blocks?
+		private async Task<Dictionary<uint, string>> ReadDBCStringBlock(DBCHeader header)
+		{
+			Dictionary<uint, string> stringMap = new Dictionary<uint, string>(1000);
+			DBCStream.Position = header.StartStringPosition;
+			byte[] bytes = new byte[DBCStream.Length - DBCStream.Position];
+
+			await ReadBytesIntoArrayFromStream(bytes);
+			DefaultStreamReaderStrategyAsync stringReader = new DefaultStreamReaderStrategyAsync(bytes);
+
+			for(int currentOffset = 0; currentOffset < bytes.Length;)
+			{
+				string readString = (await Serializer.DeserializeAsync<StringDBC>(stringReader)).StringValue;
+
+				stringMap.Add((uint)currentOffset, readString);
+
+				//We must move the offset forward length + null terminator
+				currentOffset += readString.Length + 1;
+			}
+
+			return stringMap;
+		}
+	}
+}
