68b2a286a92bd005faf14a19cebbd9cceb982c68
HelloKitty <andrew.blakely@ymail.com>
2020-04-09 01:10:29 -0500

    Added AreaTeams enum

---
+++ src/FreecraftCore.API.Data/Core/Map/AreaTeams.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreecraftCore
+{
+	[Flags]
+	public enum AreaTeams
+	{
+		AREATEAM_NONE = 0,
+		AREATEAM_ALLY = 2,
+		AREATEAM_HORDE = 4,
+		AREATEAM_ANY = 6
+	}
+}
