ad66daed4895f90baf666642d567ad282a875fcd
HelloKitty <andrew.blakely@ymail.com>
2020-04-13 13:27:38 -0500

    Added CharacterRaceFlags

---
+++ src/FreecraftCore.API.Data/Core/Race/CharacterRaceFlags.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreecraftCore
+{
+	//Based: https://wowdev.wiki/DB/ChrRaces
+	[Flags]
+	public enum CharacterRaceFlags
+	{
+		NotPlayable = 0x01,
+		BareFeet = 0x02,
+		CanMount = 0x08
+	}
+}
