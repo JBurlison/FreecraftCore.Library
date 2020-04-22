dfb4458d7c52ad799871d4ddbe7018bd3ff60d43
HelloKitty <andrew.blakely@ymail.com>
2020-04-22 18:59:19 -0500

    Added FactionGroup enum

---
+++ src/FreecraftCore.API.Data/Core/Faction/FactionGroup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Based on FactionMasks
+	/// </summary>
+	public enum FactionGroup
+	{
+		/// <summary>
+		/// Player
+		/// </summary>
+		PLAYER = 1,
+
+		/// <summary>
+		/// Player or creature from alliance team
+		/// </summary>
+		ALLIANCE = 2,
+
+		/// <summary>
+		/// Player or creature from horde team
+		/// </summary>
+		HORDE = 3,
+
+		/// <summary>
+		/// Aggressive creature from monster team
+		/// </summary>
+		MONSTER = 4
+	}
+
+	/// <summary>
+	/// based 
+	/// </summary>
+	[Flags]
+	public enum FactionGroupMask
+	{
+		PLAYER = 1 << FactionGroup.PLAYER - 1,
+		ALLIANCE = 1 << FactionGroup.ALLIANCE - 1,
+		HORDE = 1 << FactionGroup.HORDE - 1,
+		MONSTER = 1 << FactionGroup.MONSTER - 1,
+	};
+}
