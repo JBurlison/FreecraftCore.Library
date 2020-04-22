e2aa34692f2af09d23bd815531b9fca3a1c5f933
HelloKitty <andrew.blakely@ymail.com>
2020-04-22 18:44:29 -0500

    Added FactionTemplateFlags

---
+++ src/FreecraftCore.API.Data/Core/Faction/FactionTemplateFlags.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreecraftCore
+{
+	[Flags]
+	public enum FactionTemplateFlags
+	{
+		/// <summary>
+		/// Flagged for PvP
+		/// </summary>
+		FLAG_PVP = 0x00000800,
+
+		/// <summary>
+		/// Faction will attack players that were involved in PvP combats
+		/// </summary>
+		FLAG_CONTESTED_GUARD = 0x00001000,
+
+		FLAG_HOSTILE_BY_DEFAULT = 0x00002000
+	};
+}
