9af4845ee6ca09f4d262bd80a07aa250e554d8bb
HelloKitty <andrew.blakely@ymail.com>
2020-04-22 15:40:27 -0500

    Added ReputationRate

---
+++ src/FreecraftCore.API.Data/DBC/Shared/ReputationRate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace FreecraftCore
+{
+	[Owned]
+	[WireDataContract]
+	public sealed class ReputationRate
+	{
+		[WireMember(1)]
+		public Vector4<CharacterRaceMask> RaceMask { get; private set; }
+
+		[WireMember(2)]
+		public Vector4<CharacterClassMask> ClassMask { get; private set; }
+
+		[WireMember(3)]
+		public Vector4<int> BaseReputation { get; private set; }
+
+		[WireMember(4)]
+		public Vector4<FactionFlags> FactionFlag { get; private set; }
+
+		public ReputationRate([NotNull] Vector4<CharacterRaceMask> raceMask, [NotNull] Vector4<CharacterClassMask> classMask, [NotNull] Vector4<int> baseReputation, [NotNull] Vector4<FactionFlags> factionFlag)
+		{
+			RaceMask = raceMask ?? throw new ArgumentNullException(nameof(raceMask));
+			ClassMask = classMask ?? throw new ArgumentNullException(nameof(classMask));
+			BaseReputation = baseReputation ?? throw new ArgumentNullException(nameof(baseReputation));
+			FactionFlag = factionFlag ?? throw new ArgumentNullException(nameof(factionFlag));
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		protected ReputationRate()
+		{
+			
+		}
+	}
+}
