c468c7cf79fe40a62d7643515f16de2cfcf3b714
HelloKitty <andrew.blakely@ymail.com>
2020-04-22 15:33:12 -0500

    Added CharacterClassMask

--- src/FreecraftCore.API.Data/Core/Character/CharacterClass.cs
+++ src/FreecraftCore.API.Data/Core/Character/CharacterClass.cs
@@ -1,4 +1,5 @@
-﻿using FreecraftCore.Serializer;
+﻿using System;
+using FreecraftCore.Serializer;
 
 namespace FreecraftCore
 {
@@ -18,5 +19,22 @@ namespace FreecraftCore
 		Mage = 8,
 		Warlock = 9,
 		Druid = 11,
+	}
+
+	[Flags]
+	[WireDataContract]
+	public enum CharacterClassMask : int
+	{
+		None = 0,
+		Warrior = 1 << CharacterClass.Warrior - 1,
+		Paladin = 1 << CharacterClass.Paladin - 1,
+		Hunter = 1 << CharacterClass.Hunter - 1,
+		Rogue = 1 << CharacterClass.Rogue - 1,
+		Priest = 1 << CharacterClass.Priest - 1,
+		DeathKnight = 1 << CharacterClass.DeathKnight - 1,
+		Shaman = 1 << CharacterClass.Shaman - 1,
+		Mage = 1 << CharacterClass.Mage - 1,
+		Warlock = 1 << CharacterClass.Warlock - 1,
+		Druid = 1 << CharacterClass.Druid - 1,
 	}
 }
