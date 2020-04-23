4fb53d2c6512202d2a86150009ce6df4365481b5
HelloKitty <andrew.blakely@ymail.com>
2020-04-23 16:23:12 -0500

    Added Violencelevels enum

---
+++ src/FreecraftCore.API.Data/Core/Misc/Violencelevels.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreecraftCore
+{
+	public static class ViolenceLevelConstants
+	{
+		public const int NUMVIOLENCELEVELS = 3;
+	}
+
+	public enum Violencelevels
+	{
+		NONE = 0,
+		MEDIUM = 1,
+		HIGH = 2,
+	}
+}
