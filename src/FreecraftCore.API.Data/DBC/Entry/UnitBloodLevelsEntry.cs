c6f91e41eff126049adb5a60ad6bfe9907c2e710
HelloKitty <andrew.blakely@ymail.com>
2020-04-23 16:36:04 -0500

    Fixed ViolenceLevel in UnitBloodLevelsEntry

--- src/FreecraftCore.API.Data/DBC/Entry/UnitBloodLevelsEntry.cs
+++ src/FreecraftCore.API.Data/DBC/Entry/UnitBloodLevelsEntry.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
@@ -25,9 +26,9 @@ namespace FreecraftCore
 		public int UnitBloodLevelId { get; private set; }
 
 		[WireMember(2)]
-		public Vector3<Violencelevels> ViolenceLevel { get; private set; }
+		public Vector3<int> ViolenceLevel { get; private set; }
 
-		public UnitBloodLevelsEntry(int unitBloodLevelId, [NotNull] Vector3<Violencelevels> violenceLevel)
+		public UnitBloodLevelsEntry(int unitBloodLevelId, [NotNull] Vector3<int> violenceLevel)
 		{
 			UnitBloodLevelId = unitBloodLevelId;
 			ViolenceLevel = violenceLevel ?? throw new ArgumentNullException(nameof(violenceLevel));
@@ -39,6 +40,17 @@ namespace FreecraftCore
 		public UnitBloodLevelsEntry()
 		{
 			
+		}
+	}
+
+	public static class UnitBloodLevelsEntryExtensions
+	{
+		public static int GetViolenceLevel([NotNull] this UnitBloodLevelsEntry entry, Violencelevels level)
+		{
+			if (entry == null) throw new ArgumentNullException(nameof(entry));
+			if (!Enum.IsDefined(typeof(Violencelevels), level)) throw new InvalidEnumArgumentException(nameof(level), (int) level, typeof(Violencelevels));
+
+			return entry.ViolenceLevel[(int) level];
 		}
 	}
 }
