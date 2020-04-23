ec1b7cf22d9f1cb9ef29c45461b6c0fa653598ed
HelloKitty <andrew.blakely@ymail.com>
2020-04-23 16:24:22 -0500

    Added UnitBloodLevelsEntry DBC

---
+++ src/FreecraftCore.API.Data/DBC/Entry/UnitBloodLevelsEntry.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+using Newtonsoft.Json;
+
+namespace FreecraftCore
+{
+	//UnitBloodLevels.dbc
+	[DBC]
+	[WireDataContract]
+	[JsonObject]
+	[Table("UnitBloodLevels")]
+	public sealed class UnitBloodLevelsEntry : IDBCEntryIdentifiable
+	{
+		/// <inheritdoc />
+		[JsonIgnore]
+		public uint EntryId => (uint)UnitBloodLevelId;
+
+		[Key]
+		[WireMember(1)]
+		public int UnitBloodLevelId { get; private set; }
+
+		[WireMember(2)]
+		public Vector3<Violencelevels> ViolenceLevel { get; private set; }
+
+		public UnitBloodLevelsEntry(int unitBloodLevelId, [NotNull] Vector3<Violencelevels> violenceLevel)
+		{
+			UnitBloodLevelId = unitBloodLevelId;
+			ViolenceLevel = violenceLevel ?? throw new ArgumentNullException(nameof(violenceLevel));
+		}
+
+		/// <summary>
+		/// Serializer ctor
+		/// </summary>
+		public UnitBloodLevelsEntry()
+		{
+			
+		}
+	}
+}
