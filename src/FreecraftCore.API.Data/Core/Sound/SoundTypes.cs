c5891c884f84e63b4d84607a084551a01fe535dd
HelloKitty <andrew.blakely@ymail.com>
2020-04-23 18:56:22 -0500

    Added SoundTypes enum

---
+++ src/FreecraftCore.API.Data/Core/Sound/SoundTypes.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreecraftCore
+{
+	//From: https://wowdev.wiki/DB/SoundEntries
+	public enum SoundTypes
+	{
+		Miscellaneous = 0,
+		Spells = 1,
+		UI = 2,
+		Footsteps = 3,
+		Weapons_Impact = 4,
+		Weapons_Miss = 6,
+		Greetings = 7,
+		Casting = 8,
+		PickUpDown = 9,
+		NPC_Combat = 10,
+		Errors = 12,
+		Birds = 13,
+		Objects = 14,
+		Death = 16,
+		NPC_Greetings = 17,
+		Test = 18,
+		Armor_Foley = 19,
+		Footsteps2 = 20,
+		Water_Character = 21,
+		Water_Liquid = 22,
+		Tradeskills = 23,
+		Doodads = 25,
+		Spell_Fizzle = 26,
+		NPC_Loops = 27,
+		Zone_Music = 28,
+		Emotes = 29,
+		Narration_Music = 30,
+		Narration = 31,
+		Zone_Ambience = 50
+	}
+}
