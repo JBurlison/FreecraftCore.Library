505e1a8c31876b16353fe856c0cced5e16f9d415
HelloKitty <andrew.blakely@ymail.com>
2020-04-23 16:59:47 -0500

    Added some camera enums

---
+++ src/FreecraftCore.API.Data/Core/Misc/Camera.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreecraftCore
+{
+	public enum CGCameraShakeType
+	{
+		SINE = 0,
+		DECAYED_SINE = 1,
+	}
+
+	public enum CGCameraDir
+	{
+		FORWARD = 0,
+		RIGHT = 1,
+		UP = 2,
+	};
+}
