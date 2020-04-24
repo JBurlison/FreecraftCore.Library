59047dd9d9e9b0325e1f92082c75130a34d08fb8
HelloKitty <andrew.blakely@ymail.com>
2020-04-23 20:42:02 -0500

    Added CreatureImpactSound enum

---
+++ src/FreecraftCore.API.Data/Core/Creature/CreatureImpactSound.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreecraftCore
+{
+	public static class CreatureImpactSoundExtensions
+	{
+		/// <summary>
+		/// s_creatureIpactSounds
+		/// </summary>
+		public static readonly CreatureImpactSound[] CreatureImpactSoundArray = new CreatureImpactSound[]{ CreatureImpactSound.FLESH, CreatureImpactSound.STONE, CreatureImpactSound.WOOD, CreatureImpactSound.ETHEREAL };
+	}
+
+	//See: https://wowdev.wiki/DB/CreatureSoundData
+	/// <summary>
+	/// used by ? CGUnit_C::GetImpactType
+	/// </summary>
+	public enum CreatureImpactSound
+	{
+		FLESH = 0, 
+		STONE = 8, 
+		WOOD = 7, 
+		ETHEREAL = 9
+	};
+}
