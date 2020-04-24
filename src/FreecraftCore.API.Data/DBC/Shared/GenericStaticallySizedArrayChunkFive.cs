29c93db7a7974ee4f420f0efce5149d9e21db48a
HelloKitty <andrew.blakely@ymail.com>
2020-04-23 20:24:50 -0500

    Added GenericStaticallySizedArrayChunkFive

---
+++ src/FreecraftCore.API.Data/DBC/Shared/GenericStaticallySizedArrayChunkFive.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using FreecraftCore.Serializer;
+using Microsoft.EntityFrameworkCore;
+
+namespace FreecraftCore
+{
+	//Exists because EF Core doesn't support arrays.
+	[Owned]
+	[WireDataContract]
+	public sealed class GenericStaticallySizedArrayChunkFive<T>// : IEnumerable<T>, IReadOnlyList<T> TODO: Owned types cannot have interfaces
+	{
+		[WireMember(1)]
+		public T One { get; private set; }
+
+		[WireMember(2)]
+		public T Two { get; private set; }
+
+		[WireMember(3)]
+		public T Three { get; private set; }
+
+		[WireMember(4)]
+		public T Four { get; private set; }
+
+		[WireMember(5)]
+		public T Five { get; private set; }
+
+		public GenericStaticallySizedArrayChunkFive(T one, T two, T three, T four, T five)
+		{
+			One = one;
+			Two = two;
+			Three = three;
+			Four = four;
+			Five = five;
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		public GenericStaticallySizedArrayChunkFive()
+		{
+			
+		}
+
+		public IEnumerator<T> GetEnumerator()
+		{
+			yield return One;
+			yield return Two;
+			yield return Three;
+			yield return Four;
+			yield return Five;
+		}
+
+		public int Count => 5;
+
+		public T this[int index]
+		{
+			get
+			{
+				switch (index)
+				{
+					case 1:
+						return One;
+					case 2:
+						return Two;
+					case 3:
+						return Three;
+					case 4:
+						return Four;
+					case 5:
+						return Five;
+					default:
+						throw new IndexOutOfRangeException($"Index {index} is out of range. Max range of: {Count}");
+				}
+			}
+		}
+	}
+}
