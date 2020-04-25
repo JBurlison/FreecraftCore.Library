6d5ab4560b125dd3b79845482890897bee88f551
HelloKitty <andrew.blakely@ymail.com>
2020-04-25 17:11:48 -0500

    Added CreatureDisplayInfoExtraFlags enum

---
+++ src/FreecraftCore.API.Data/Core/Creature/CreatureDisplayInfoExtraFlags.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreecraftCore
+{
+	public enum CreatureDisplayInfoExtraFlags
+	{
+		None,
+		CanEquipPlayerEquipment = 1 << 0,
+	}
+}
