c287926dfe8083bc17f8808a09c1669c87b506d3
HelloKitty <andrew.blakely@ymail.com>
2020-04-25 20:31:16 -0500

    Added NpcSounds enum

---
+++ src/FreecraftCore.API.Data/Core/Creature/NpcSounds.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreecraftCore
+{
+	public enum NpcSounds
+	{
+		/// <summary>
+		/// Greetings 
+		/// </summary>
+		HELLO = 0,
+
+		/// <summary>
+		/// Farewells
+		/// </summary>
+		GOODBYE = 1,
+
+		/// <summary>
+		/// Annoyed.
+		/// </summary>
+		PISSED = 2,
+
+		/// <summary>
+		/// Unknown/TODO.
+		/// Wiki says: Q: stores last sound file? memory allocation when table is loaded.
+		/// </summary>
+		ACK = 3,
+
+		//NUM_NPCSOUNDS = 4
+	};
+}
