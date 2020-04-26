862b2dcdcade956566cd2f7d580dec1863a14228
HelloKitty <andrew.blakely@ymail.com>
2020-04-25 20:02:22 -0500

    Added BloodSpurtSizes enum

---
+++ src/FreecraftCore.API.Data/Core/Creature/BloodSpurtSizes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreecraftCore
+{
+	public enum BloodSpurtSizes
+	{
+		SMALL = 0,
+		LARGE = 1,
+		//NUM_BLOODSPURTSIZES = 2
+	}
+}
