22c70506e44c20547469e1eef04f7bd57645c835
HelloKitty <andrew.blakely@ymail.com>
2020-04-26 19:20:23 -0500

    Added CreatureTypeDefinitionFlags enum

---
+++ src/FreecraftCore.API.Data/Core/Creature/CreatureTypeDefinitionFlags.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Not to be confused with <see cref="CreatureTypeFlags"/> which is NOT a DBC-based enum.
+	/// This enum is from <see cref="CreatureTypeEntry{TStringType}"/> the flags column.
+	/// </summary>
+	[Flags]
+	public enum CreatureTypeDefinitionFlags
+	{
+		None = 0,
+		
+		/// <summary>
+		/// Flag indicates that the creature type should reward no experience.
+		/// </summary>
+		NoExperience = 1 << 0,
+	}
+}
