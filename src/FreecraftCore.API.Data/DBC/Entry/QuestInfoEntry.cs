c16e41f3d0f25e57cd756599ac8c33289db10838
HelloKitty <andrew.blakely@ymail.com>
2020-04-29 08:05:46 -0500

    Added QuestInfo DBC Model

---
+++ src/FreecraftCore.API.Data/DBC/Entry/QuestInfoEntry.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+using Newtonsoft.Json;
+
+namespace FreecraftCore
+{
+	//QuestInfo.dbc
+	/// <summary>
+	/// Table model for QuestInfo.dbc
+	/// https://wowdev.wiki/DB/QuestInfo
+	/// </summary>
+	[DBC]
+	[WireDataContract]
+	[JsonObject]
+	[StringDBC(typeof(QuestInfoEntry<>))]
+	[Table("QuestInfo")]
+	public class QuestInfoEntry<TStringType> : IDBCEntryIdentifiable
+	{
+		[NotMapped]
+		[JsonIgnore]
+		public uint EntryId => (uint) QuestInfoId;
+
+		[Key]
+		[WireMember(1)]
+		public int QuestInfoId { get; private set; }
+
+		[WireMember(2)]
+		public LocalizedStringDBC<TStringType> Name { get; private set; }
+
+		public QuestInfoEntry(int questInfoId, [NotNull] LocalizedStringDBC<TStringType> name)
+		{
+			if (questInfoId <= 0) throw new ArgumentOutOfRangeException(nameof(questInfoId));
+
+			QuestInfoId = questInfoId;
+			Name = name ?? throw new ArgumentNullException(nameof(name));
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		public QuestInfoEntry()
+		{
+			
+		}
+	}
+}
