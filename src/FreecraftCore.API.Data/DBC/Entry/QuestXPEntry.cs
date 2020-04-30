e264e8ee8df900159b81589b38c5d8ee77529a1d
HelloKitty <andrew.blakely@ymail.com>
2020-04-30 13:15:26 -0500

    Added QuestXP DBC Model

---
+++ src/FreecraftCore.API.Data/DBC/Entry/QuestXPEntry.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+using Newtonsoft.Json;
+
+namespace FreecraftCore
+{
+	//QuestXP.dbc
+	/// <summary>
+	/// Table model for QuestXP.dbc
+	/// https://wowdev.wiki/DB/QuestXP
+	/// Poorly named. It's basically base-reward based on level and quest difficulty.
+	/// </summary>
+	[DBC]
+	[WireDataContract]
+	[JsonObject]
+	[Table("QuestXP")]
+	public class QuestXPEntry : IDBCEntryIdentifiable
+	{
+		[NotMapped]
+		[JsonIgnore]
+		public uint EntryId => (uint) Level;
+
+		[Key]
+		[WireMember(1)]
+		public int Level { get; private set; }
+
+		[WireMember(2)]
+		public GenericStaticallySizedArrayChunkTen<int> DifficultyReward { get; private set; }
+
+		public QuestXPEntry(int level, [NotNull] GenericStaticallySizedArrayChunkTen<int> difficultyReward)
+		{
+			if (level <= 0) throw new ArgumentOutOfRangeException(nameof(level));
+
+			Level = level;
+			DifficultyReward = difficultyReward ?? throw new ArgumentNullException(nameof(difficultyReward));
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		public QuestXPEntry()
+		{
+			
+		}
+	}
+}
