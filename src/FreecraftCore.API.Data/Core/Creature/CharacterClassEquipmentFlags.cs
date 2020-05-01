bd77fbab5cd466bb957082299288a4272f05921a
HelloKitty <andrew.blakely@ymail.com>
2020-05-01 17:53:39 -0500

    Added CharacterClassEquipmentFlags enum

---
+++ src/FreecraftCore.API.Data/Core/Creature/CharacterClassEquipmentFlags.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreecraftCore
+{
+	[Flags]
+	public enum CharacterClassEquipmentFlags
+	{
+		CanWearLeather = 1 << 0,
+		CanWearCloth = 1 << 1,
+		CanSummonPets = 1 << 2,
+		UsesRelic = 1 << 3,
+		CanWearMail = 1 << 4,
+		CanWearPlate = 1 << 5
+	}
+}
