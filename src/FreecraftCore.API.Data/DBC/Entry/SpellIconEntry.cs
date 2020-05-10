2d24e2a27068832fb059e229f8a4a2be9a7608d8
HelloKitty <andrew.blakely@ymail.com>
2020-05-10 11:06:58 -0500

    Added SpellIcon DBC Model

---
+++ src/FreecraftCore.API.Data/DBC/Entry/SpellIconEntry.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Globalization;
+using System.Numerics;
+using System.Text;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+using Newtonsoft.Json;
+
+namespace FreecraftCore
+{
+	//SpellIcon.dbc
+	/// <summary>
+	/// Table model for SpellIcon.dbc
+	/// https://wowdev.wiki/DB/SpellIcon
+	/// Not only for spells.
+	/// </summary>
+	[DBC]
+	[WireDataContract]
+	[JsonObject]
+	[StringDBC(typeof(SpellIconEntry<>))]
+	[Table("SpellIcon")]
+	public class SpellIconEntry<TStringType> : IDBCEntryIdentifiable
+		where TStringType : class
+	{
+		[NotMapped]
+		[JsonIgnore]
+		public uint EntryId => (uint) IconId;
+
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		[WireMember(1)]
+		public int IconId { get; private set; }
+
+		/// <summary>
+		/// There are no extensions (.blp) in these. They mostly match "Interface\Icons\[a-z_0-9]" but there are exceptions.
+		/// </summary>
+		[WireMember(2)]
+		public TStringType TextureFileName { get; private set; }
+
+		public SpellIconEntry(int iconId, [NotNull] TStringType textureFileName)
+		{
+			if (iconId <= 0) throw new ArgumentOutOfRangeException(nameof(iconId));
+
+			IconId = iconId;
+			TextureFileName = textureFileName ?? throw new ArgumentNullException(nameof(textureFileName));
+		}
+
+		//DBC Tools requires this to be public.
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		public SpellIconEntry()
+		{
+			
+		}
+	}
+}
