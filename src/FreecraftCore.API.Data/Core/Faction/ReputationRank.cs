345540008f377d925944ca4d979001463621c526
HelloKitty <andrew.blakely@ymail.com>
2020-05-22 15:39:08 -0500

    Added ReputationRank enum

---
+++ src/FreecraftCore.API.Data/Core/Faction/ReputationRank.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// See: TrinityCore's enum ReputationRank.
+	/// </summary>
+	public enum ReputationRank
+	{
+		HATED = 0,
+		HOSTILE = 1,
+		UNFRIENDLY = 2,
+		NEUTRAL = 3,
+		FRIENDLY = 4,
+		HONORED = 5,
+		REVERED = 6,
+		EXALTED = 7
+	};
+}
