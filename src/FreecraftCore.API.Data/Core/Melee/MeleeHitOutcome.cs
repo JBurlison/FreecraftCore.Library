84d165f079d2d115bae1d4208e63dc5df548f851
HelloKitty <andrew.blakely@ymail.com>
2020-05-24 19:56:33 -0500

    Added MeleeHitOutcome enum

---
+++ src/FreecraftCore.API.Data/Core/Melee/MeleeHitOutcome.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreecraftCore
+{
+	public enum MeleeHitOutcome : byte
+	{
+		MELEE_HIT_EVADE, 
+		MELEE_HIT_MISS, 
+		MELEE_HIT_DODGE, 
+		MELEE_HIT_BLOCK, 
+		MELEE_HIT_PARRY,
+		MELEE_HIT_GLANCING, 
+		MELEE_HIT_CRIT, 
+		MELEE_HIT_CRUSHING, 
+		MELEE_HIT_NORMAL
+	};
+}
