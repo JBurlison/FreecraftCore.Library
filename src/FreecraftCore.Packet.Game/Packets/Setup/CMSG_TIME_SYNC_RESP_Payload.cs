d8af90c9505ef1b0f46bb4bf027db5d28af188b8
HelloKitty <andrew.blakely@ymail.com>
2020-06-03 17:32:51 -0500

    Added CMSG_TIME_SYNC_RESP payload

---
+++ src/FreecraftCore.Packet.Game/Packets/Setup/CMSG_TIME_SYNC_RESP_Payload.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Server message sent to the client to start a time sync exchange.
+	/// </summary>
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.CMSG_TIME_SYNC_RESP)]
+	public sealed class CMSG_TIME_SYNC_RESP_Payload : GamePacketPayload
+	{
+		/// <summary>
+		/// Represents the counter/index for which sync request
+		/// this response packet is responding to.
+		/// </summary>
+		[WireMember(1)]
+		public int SynchronizationCounter { get; private set; }
+
+		/// <summary>
+		/// Represents the time at which the client has handled
+		/// the matching <see cref="SMSG_TIME_SYNC_REQ_Payload"/>.
+		/// </summary>
+		[WireMember(2)]
+		public uint ClientTimestamp { get; private set; }
+
+		public CMSG_TIME_SYNC_RESP_Payload(int synchronizationCounter, uint clientTimestamp)
+		{
+			if (synchronizationCounter < 0) throw new ArgumentOutOfRangeException(nameof(synchronizationCounter));
+			if (clientTimestamp == 0) throw new ArgumentOutOfRangeException(nameof(clientTimestamp));
+
+			SynchronizationCounter = synchronizationCounter;
+			ClientTimestamp = clientTimestamp;
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		private CMSG_TIME_SYNC_RESP_Payload()
+		{
+
+		}
+	}
+}
