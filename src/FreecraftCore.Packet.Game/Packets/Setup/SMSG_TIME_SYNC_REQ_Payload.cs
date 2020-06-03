03319b58f1509806156f7e70f83b88a416a0097e
HelloKitty <andrew.blakely@ymail.com>
2020-06-03 17:27:13 -0500

    Added payload for SMSG_TIME_SYNC_REQ

---
+++ src/FreecraftCore.Packet.Game/Packets/Setup/SMSG_TIME_SYNC_REQ_Payload.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Server message sent to the client to start a time sync exchange.
+	/// </summary>
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.SMSG_TIME_SYNC_REQ)]
+	public sealed class SMSG_TIME_SYNC_REQ_Payload : GamePacketPayload
+	{
+		/// <summary>
+		/// Represents the counter for how many
+		/// times the client has sync'd.
+		/// Incremented each time a sync is sent.
+		/// </summary>
+		[WireMember(1)]
+		public int SynchronizationCounter { get; private set; }
+
+		public SMSG_TIME_SYNC_REQ_Payload(int synchronizationCounter)
+		{
+			if (synchronizationCounter < 0) throw new ArgumentOutOfRangeException(nameof(synchronizationCounter));
+
+			SynchronizationCounter = synchronizationCounter;
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		private SMSG_TIME_SYNC_REQ_Payload()
+		{
+			
+		}
+	}
+}
