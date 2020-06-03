adb064b03814875814f6793ff5c28b4116522ee2
HelloKitty <andrew.blakely@ymail.com>
2020-06-03 18:15:57 -0500

    Added CMSG_QUERY_TIME Payload

---
+++ src/FreecraftCore.Packet.Game/Packets/Setup/CMSG_QUERY_TIME_Payload.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Empty request query payload send to the server
+	/// to request the absolute timestamp.
+	/// Server should return with: <see cref="SMSG_QUERY_TIME_RESPONSE_Payload"/>
+	/// </summary>
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.CMSG_QUERY_TIME)]
+	public sealed class CMSG_QUERY_TIME_Payload : GamePacketPayload
+	{
+		/// <summary>
+		/// Creates a new time query request.
+		/// </summary>
+		public CMSG_QUERY_TIME_Payload()
+		{
+			
+		}
+	}
+}
