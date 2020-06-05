ddb111da07897a1a4ffd35d57f94d8177c153dd8
HelloKitty <andrew.blakely@ymail.com>
2020-06-05 03:28:54 -0500

    Added SecureAddonStatus enum

---
+++ src/FreecraftCore.API.Data/Core/Addon/SecureAddonStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreecraftCore
+{
+	public enum SecureAddonStatus : byte
+	{
+		BANNED = 0,
+		SECURE_VISIBLE = 1,
+		SECURE_HIDDEN = 2
+	};
+}
