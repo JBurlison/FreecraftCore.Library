d87c2830ce3249e6cd5c15db008f307541b57c90
HelloKitty <andrew.blakely@ymail.com>
2020-06-05 21:16:22 -0500

    Added CMSG_LOGOUT_REQUEST payload

---
+++ src/FreecraftCore.Packet.Game/Packets/Setup/CMSG_LOGOUT_REQUEST_Payload.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+
+namespace FreecraftCore
+{
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.CMSG_LOGOUT_REQUEST)]
+	public sealed class CMSG_LOGOUT_REQUEST_Payload : GamePacketPayload
+	{
+		/// <summary>
+		/// Creates a new request to logout.
+		/// </summary>
+		public CMSG_LOGOUT_REQUEST_Payload()
+		{
+			
+		}
+	}
+}
