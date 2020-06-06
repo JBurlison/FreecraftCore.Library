349bc89c02d0376ca6d06fe7c60fe54f14c90b6f
HelloKitty <andrew.blakely@ymail.com>
2020-06-05 21:35:06 -0500

    Added LogoutResultCode

---
+++ src/FreecraftCore.API.Data/Core/Misc/LogoutResultCode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreecraftCore
+{
+	public enum LogoutResultCode
+	{
+		Success = 0,
+		InCombat = 1,
+		GeneralError = 2,
+		Falling = 3,
+	}
+}
