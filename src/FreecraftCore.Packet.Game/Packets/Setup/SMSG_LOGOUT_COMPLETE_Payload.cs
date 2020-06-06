380cbc227384904b06ab05046758e562214abef7
HelloKitty <andrew.blakely@ymail.com>
2020-06-06 00:09:40 -0500

    Added SMSG_LOGOUT_COMPLETE payload

---
+++ src/FreecraftCore.Packet.Game/Packets/Setup/SMSG_LOGOUT_COMPLETE_Payload.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using FreecraftCore.Serializer;
+
+namespace FreecraftCore
+{
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.SMSG_LOGOUT_COMPLETE)]
+	public sealed class SMSG_LOGOUT_COMPLETE_Payload : GamePacketPayload
+	{
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		public SMSG_LOGOUT_COMPLETE_Payload()
+		{
+			
+		}
+	}
+}
