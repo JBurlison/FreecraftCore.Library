93a839a97ccc3a258247b5716f1f85612c785693
HelloKitty <andrew.blakely@ymail.com>
2020-06-07 03:51:31 -0500

    Added SMSG_SPELL_START Payload

---
+++ src/FreecraftCore.Packet.Game/Packets/Spell/SMSG_SPELL_START_Payload.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+
+namespace FreecraftCore
+{
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.SMSG_SPELL_START)]
+	public sealed class SMSG_SPELL_START_Payload : GamePacketPayload
+	{
+		[WireMember(1)]
+		public SpellCastData CastData { get; internal set; }
+
+		public SMSG_SPELL_START_Payload([NotNull] SpellCastData castData)
+		{
+			CastData = castData ?? throw new ArgumentNullException(nameof(castData));
+		}
+
+		internal SMSG_SPELL_START_Payload()
+		{
+			
+		}
+	}
+}
