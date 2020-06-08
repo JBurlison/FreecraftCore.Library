b354d860a1b0ce4c59236db50075a637d0b1c74b
HelloKitty <andrew.blakely@ymail.com>
2020-06-07 22:04:03 -0500

    Added SMSG_SPELL_FAILURE packet

---
+++ src/FreecraftCore.Packet.Game/Packets/Spell/SMSG_SPELL_FAILURE_Payload.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+
+namespace FreecraftCore
+{
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.SMSG_SPELL_FAILURE)]
+	public sealed class SMSG_SPELL_FAILURE_Payload : GamePacketPayload
+	{
+		[WireMember(1)]
+		public SpellFailureData FailureData { get; internal set; }
+
+		public SMSG_SPELL_FAILURE_Payload([NotNull] SpellFailureData failureData)
+		{
+			FailureData = failureData ?? throw new ArgumentNullException(nameof(failureData));
+		}
+
+		internal SMSG_SPELL_FAILURE_Payload()
+		{
+			
+		}
+	}
+}
