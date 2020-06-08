7499b8cb2e509821504b603e9ae20245fc53c88b
HelloKitty <andrew.blakely@ymail.com>
2020-06-08 03:09:29 -0500

    Added CMSG_SET_SELECTION packet

---
+++ src/FreecraftCore.Packet.Game/Packets/Unit/CMSG_SET_SELECTION_Payload.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+
+namespace FreecraftCore
+{
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.CMSG_SET_SELECTION)]
+	public sealed class CMSG_SET_SELECTION_Payload : GamePacketPayload
+	{
+		[WireMember(1)]
+		public ObjectGuid Target { get; internal set; }
+
+		public CMSG_SET_SELECTION_Payload([NotNull] ObjectGuid target)
+		{
+			Target = target ?? throw new ArgumentNullException(nameof(target));
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		internal CMSG_SET_SELECTION_Payload()
+		{
+			
+		}
+	}
+}
