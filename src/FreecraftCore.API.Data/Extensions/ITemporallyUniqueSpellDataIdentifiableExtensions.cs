38dad635ff478ef20d6bdcc41b25e7e5747efb27
HelloKitty <andrew.blakely@ymail.com>
2020-06-07 22:02:37 -0500

    Added ITemporallyUniqueSpellDataIdentifiableExtensions

---
+++ src/FreecraftCore.API.Data/Extensions/ITemporallyUniqueSpellDataIdentifiableExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using JetBrains.Annotations;
+
+namespace FreecraftCore
+{
+	public static class ITemporallyUniqueSpellDataIdentifiableExtensions
+	{
+		/// <summary>
+		/// Computes a temporally unique identifier from the provided <see cref="ITemporallyUniqueSpellDataIdentifiable"/> data.
+		/// </summary>
+		/// <param name="data">Temporally unique spell data.</param>
+		/// <returns>Long key that will be unique within a short time window.</returns>
+		public static long CalculateTemporallyUniqueKey([NotNull] this ITemporallyUniqueSpellDataIdentifiable data)
+		{
+			if(data == null) throw new ArgumentNullException(nameof(data));
+
+			return (long)data.SpellCastCount << 32 | (long)data.SpellId;
+		}
+	}
+}
