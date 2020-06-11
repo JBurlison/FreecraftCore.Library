2cd9875f6dcfa3446188d0bc5218ed1468dbafe9
HelloKitty <andrew.blakely@ymail.com>
2020-06-11 07:43:40 -0500

    Added SMSG_NEW_WORLD Packet

---
+++ src/FreecraftCore.Packet.Game/Packets/Map/SMSG_NEW_WORLD_Payload.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Packet sent when the client should begin loading to a new map.
+	/// </summary>
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.SMSG_NEW_WORLD)]
+	public sealed class SMSG_NEW_WORLD_Payload : GamePacketPayload
+	{
+		/// <summary>
+		/// The ID of the map to teleport.
+		/// </summary>
+		[WireMember(1)]
+		public int MapId { get; internal set; }
+
+		/// <summary>
+		/// The position to teleport to.
+		/// </summary>
+		[WireMember(2)]
+		public Vector3<float> Position { get; internal set; }
+
+		/// <summary>
+		/// The orientation to teleport to.
+		/// </summary>
+		[WireMember(3)]
+		public float Orientation { get; internal set; }
+
+		public SMSG_NEW_WORLD_Payload(int mapId, [NotNull] Vector3<float> position, float orientation)
+		{
+			if (mapId <= 0) throw new ArgumentOutOfRangeException(nameof(mapId));
+
+			MapId = mapId;
+			Position = position ?? throw new ArgumentNullException(nameof(position));
+			Orientation = orientation;
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		internal SMSG_NEW_WORLD_Payload()
+		{
+
+		}
+	}
+}
