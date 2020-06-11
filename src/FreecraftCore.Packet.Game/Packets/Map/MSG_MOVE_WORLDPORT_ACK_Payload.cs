4f79f32f7c1d2dac274c0177960ecdc588beade8
HelloKitty <andrew.blakely@ymail.com>
2020-06-11 08:23:45 -0500

    Added MSG_MOVE_WORLDPORT_ACK Packet

---
+++ src/FreecraftCore.Packet.Game/Packets/Map/MSG_MOVE_WORLDPORT_ACK_Payload.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Packet sent by the client to acknowledge a map teleport.
+	/// Usually sent in response to <see cref="SMSG_NEW_WORLD_Payload"/>.
+	/// </summary>
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.MSG_MOVE_WORLDPORT_ACK)]
+	public sealed class MSG_MOVE_WORLDPORT_ACK_Payload : GamePacketPayload
+	{
+		/// <summary>
+		/// Creates a new world port acknowledgement.
+		/// </summary>
+		public MSG_MOVE_WORLDPORT_ACK_Payload()
+		{
+
+		}
+	}
+}
