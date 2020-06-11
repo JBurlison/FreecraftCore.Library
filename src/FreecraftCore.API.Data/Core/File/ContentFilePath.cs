2d03edbbd16a5c70d83d3b3340fb3fa01195a7d8
HelloKitty <andrew.blakely@ymail.com>
2020-06-11 12:19:08 -0500

    Created ContentFilePath base type

---
+++ src/FreecraftCore.API.Data/Core/File/ContentFilePath.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using JetBrains.Annotations;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Builds a path to an asset based on the provided
+	/// <see cref="FileName"/>.
+	/// </summary>
+	public abstract class ContentFilePath
+	{
+		/// <summary>
+		/// The name of the map file.
+		/// </summary>
+		protected string FileName { get; }
+
+		private Lazy<string> _Path { get; }
+
+		/// <summary>
+		/// The path to the map file asset.
+		/// </summary>
+		public string Path => _Path.Value;
+
+		protected ContentFilePath([NotNull] string fileName)
+		{
+			if(string.IsNullOrWhiteSpace(fileName)) throw new ArgumentException("Value cannot be null or whitespace.", nameof(fileName));
+
+			FileName = fileName;
+			_Path = new Lazy<string>(Build, true);
+		}
+
+		public static implicit operator string(ContentFilePath path)
+		{
+			return path?.Path;
+		}
+
+		protected abstract string Build();
+	}
+}
---
+++ src/FreecraftCore.API.Data/Core/File/MapFilePath.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using JetBrains.Annotations;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Map file implementation of <see cref="ContentFilePath"/>.
+	/// </summary>
+	public class MapFilePath : ContentFilePath
+	{
+		public MapFilePath([NotNull] string fileName)
+			: base(fileName)
+		{
+
+		}
+
+		protected override string Build()
+		{
+			return $"World/Maps/{FileName}/{FileName}";
+		}
+	}
+}
