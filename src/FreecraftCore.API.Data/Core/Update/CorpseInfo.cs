515d25d3798ac556fba6ccd3f337cf068b659287
HelloKitty <andrew.blakely@ymail.com>
2020-06-18 16:56:13 -0500

    Documented CorpseInfo

--- src/FreecraftCore.API.Data/Core/Update/CorpseInfo.cs
+++ src/FreecraftCore.API.Data/Core/Update/CorpseInfo.cs
@@ -6,18 +6,39 @@ namespace FreecraftCore
 	public sealed class CorpseInfo
 	{
 		//TODO: What is this?
+		/// <summary>
+		/// If a Transport object exists then this will be the GUID of the transport.
+		/// Otherwise Empty.
+		/// </summary>
 		[WireMember(1)]
 		public PackedGuid TransportGuid { get; internal set; }
 
+		/// <summary>
+		/// The absolute coordinate position of the object.
+		/// (Usually GameObject)
+		/// </summary>
 		[WireMember(2)]
 		public Vector3<float> GoLocation { get; internal set; }
 
+		/// <summary>
+		/// If <see cref="TransportGuid"/> exists then
+		/// this will be the transport offset.
+		/// Otherwise for some reason it's a copy of <see cref="GoLocation"/>.
+		/// </summary>
 		[WireMember(3)]
 		public Vector3<float> TransportOffset { get; internal set; }
 
+		/// <summary>
+		/// Represents Y-Axis orientation.
+		/// </summary>
 		[WireMember(4)]
 		public float Orientation { get; internal set; }
 
+		/// <summary>
+		/// If the Unit is TYPID_CORPSE then this is the corpse orientation for the unit.
+		/// Otherwise this will be 0. Will be a copy of <see cref="Orientation"/>.
+		/// Very Strange!!
+		/// </summary>
 		[WireMember(5)]
 		public float CorpseOrientation { get; internal set; }
 
@@ -31,7 +52,10 @@ namespace FreecraftCore
 			CorpseOrientation = corpseOrientation;
 		}
 
-		protected CorpseInfo()
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		internal CorpseInfo()
 		{
 			
 		}
