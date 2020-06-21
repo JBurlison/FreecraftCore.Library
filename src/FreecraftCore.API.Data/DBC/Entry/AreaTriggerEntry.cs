353993f19984db138ff64fb66dd5a62e89cadbbf
HelloKitty <andrew.blakely@ymail.com>
2020-06-21 12:41:42 -0500

    Improved AreaTrigger DBC

--- src/FreecraftCore.API.Data/DBC/Entry/AreaTriggerEntry.cs
+++ src/FreecraftCore.API.Data/DBC/Entry/AreaTriggerEntry.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 using FreecraftCore.Serializer;
+using JetBrains.Annotations;
 using Newtonsoft.Json;
 
 namespace FreecraftCore
@@ -17,7 +18,7 @@ namespace FreecraftCore
 	[WireDataContract]
 	[JsonObject]
 	[Table("AreaTrigger")]
-	public sealed class AreaTriggerEntry : IDBCEntryIdentifiable
+	public class AreaTriggerEntry : IDBCEntryIdentifiable
 	{
 		/// <inheritdoc />
 		[JsonIgnore]
@@ -34,6 +35,10 @@ namespace FreecraftCore
 		[WireMember(2)]
 		public int MapId { get; internal set; }
 
+		[JsonIgnore]
+		[ForeignKey(nameof(MapId))]
+		public virtual MapEntry<string> Map { get; internal set; }
+
 		/// <summary>
 		/// Seems to be a box of size yards with center at x,y,z.
 		/// </summary>
@@ -41,7 +46,7 @@ namespace FreecraftCore
 		public Vector3<float> Position { get; internal set; }
 
 		/// <summary>
-		/// 
+		/// Seems to be a box of size yards with center at x,y,z
 		/// </summary>
 		[WireMember(4)]
 		public float Radius { get; internal set; }
@@ -49,13 +54,17 @@ namespace FreecraftCore
 		/// <summary>
 		/// Indicates if the box is a radius-based axis-aligned bounding box trigger.
 		/// </summary>
+		[NotMapped]
 		[JsonIgnore]
 		public bool isAxisAlignedBox => Math.Abs(Radius) > 0.0001f;
 
 		//Most commonly used when size is 0, but not always
 		/// <summary>
 		/// When <see cref="Radius"/> is 0 this is sometimes used to define
-		/// a non-uniform axis-aligned bounding box. Think a rectengular prism.
+		/// a non-uniform axis-aligned bounding box. Think a rectangular prism.
+		/// BoxLength Float Most commonly used when size is 0, but not always
+		/// BoxWidth Float
+		/// BoxHeight Float
 		/// </summary>
 		[WireMember(5)]
 		public Vector3<float> UnalignedBoxDimension { get; internal set; }
@@ -67,12 +76,25 @@ namespace FreecraftCore
 		[WireMember(6)]
 		public float Orientation { get; internal set; }
 
+		public AreaTriggerEntry(int areaTriggerId, int mapId, [NotNull] Vector3<float> position, float radius, [NotNull] Vector3<float> unalignedBoxDimension, float orientation)
+		{
+			//Blizzlike DBC sometimes 0, so dumb!
+			if (mapId < 0) throw new ArgumentOutOfRangeException(nameof(mapId));
+
+			AreaTriggerId = areaTriggerId;
+			MapId = mapId;
+			Position = position ?? throw new ArgumentNullException(nameof(position));
+			Radius = radius;
+			UnalignedBoxDimension = unalignedBoxDimension ?? throw new ArgumentNullException(nameof(unalignedBoxDimension));
+			Orientation = orientation;
+		}
+
 		/// <summary>
 		/// Serializer ctor
 		/// </summary>
-		public AreaTriggerEntry()
+		internal AreaTriggerEntry()
 		{
-			
+
 		}
 	}
 }
