f44606344425780d2558b2215e18f5f742e059ef
HelloKitty <andrew.blakely@ymail.com>
2020-06-21 12:42:02 -0500

    Added CMSG_AREATRIGGER Packet

---
+++ src/FreecraftCore.Packet.Game/Packets/Interaction/CMSG_AREATRIGGER_Payload.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+
+namespace FreecraftCore
+{
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.CMSG_AREATRIGGER)]
+	public sealed class CMSG_AREATRIGGER_Payload : GamePacketPayload
+	{
+		/// <summary>
+		/// The ID of the <see cref="AreaTriggerEntry"/>.
+		/// </summary>
+		[WireMember(1)]
+		public int AreaTriggerId { get; internal set; }
+
+		public CMSG_AREATRIGGER_Payload(int areaTriggerId)
+		{
+			if (areaTriggerId <= 0) throw new ArgumentOutOfRangeException(nameof(areaTriggerId));
+
+			AreaTriggerId = areaTriggerId;
+		}
+
+		internal CMSG_AREATRIGGER_Payload()
+		{
+			
+		}
+	}
+}
