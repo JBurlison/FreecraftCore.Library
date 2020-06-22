dcd6fd6dbd71020565c18c3d9b4992e20d445a3e
HelloKitty <andrew.blakely@ymail.com>
2020-06-22 14:48:49 -0500

    Added IMovementSpeedChangePayload interface

---
+++ src/FreecraftCore.Packet.Game/Packets/Movement/Speed/IMovementSpeedChangePayload.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Contract for packets that mutate movement speed
+	/// of a specified <see cref="ObjectGuid"/>.
+	/// </summary>
+	public interface IMovementSpeedChangePayload
+	{
+		/// <summary>
+		/// The <see cref="ObjectGuid"/> of the Entity to target for this movement update.
+		/// </summary>
+		PackedGuid Target { get; }
+
+		/// <summary>
+		/// The move type movement speed to mutate.
+		/// </summary>
+		UnitMoveType MoveType { get; }
+
+		/// <summary>
+		/// The new speed to mutate the <see cref="MoveType"/> movement speed by for the <see cref="Target"/>.
+		/// </summary>
+		float Speed { get; }
+	}
+}
