81c35ef37a27b0df6e6afe9dafe86ed58462fa8c
HelloKitty <andrew.blakely@ymail.com>
2020-06-22 15:45:47 -0500

    Implemented SMSG_SPLINE_SET packet fields

--- src/FreecraftCore.Packet.Game/Packets/Movement/Speed/Spline/SMSG_SPLINE_SET_RUN_SPEED.cs
+++ src/FreecraftCore.Packet.Game/Packets/Movement/Speed/Spline/SMSG_SPLINE_SET_RUN_SPEED.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 using FreecraftCore.Serializer;
 using JetBrains.Annotations;
+using Newtonsoft.Json;
 
 namespace FreecraftCore
 {
@@ -11,8 +13,21 @@ namespace FreecraftCore
 	/// </summary>
 	[WireDataContract]
 	[GamePayloadOperationCode(NetworkOperationCode.SMSG_SPLINE_SET_RUN_SPEED)]
-	public sealed class SMSG_SPLINE_SET_RUN_SPEED_Payload : GamePacketPayload
+	public sealed class SMSG_SPLINE_SET_RUN_SPEED_Payload : GamePacketPayload, IMovementSpeedChangePayload
 	{
+		/// <inheritdoc />
+		[WireMember(1)]
+		public PackedGuid Target { get; internal set; }
+
+		/// <inheritdoc />
+		[JsonIgnore]
+		[NotMapped]
+		public UnitMoveType MoveType => UnitMoveType.MOVE_RUN;
+
+		/// <inheritdoc />
+		[WireMember(2)]
+		public float Speed { get; internal set; }
+
 		/// <summary>
 		/// Default Serializer Ctor.
 		/// </summary>
