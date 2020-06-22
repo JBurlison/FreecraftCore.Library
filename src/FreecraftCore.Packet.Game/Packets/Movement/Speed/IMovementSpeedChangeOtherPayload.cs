3f1c80962678ac478749bfdadb121f13c0386d2f
HelloKitty <andrew.blakely@ymail.com>
2020-06-22 15:31:11 -0500

    Added IMovementSpeedChangeOtherPayload interface

---
+++ src/FreecraftCore.Packet.Game/Packets/Movement/Speed/IMovementSpeedChangeOtherPayload.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Contract for packet payloads that mutate movement speed like <see cref="IMovementSpeedChangePayload"/>
+	/// but contain additional information because it's about remote players.
+	/// </summary>
+	public interface IMovementSpeedChangeOtherPayload : IMovementSpeedChangePayload
+	{
+		/// <summary>
+		/// The remote <see cref="Target"/>'s <see cref="MovementInfo"/> at the time of the movement speed change.
+		/// </summary>
+		MovementInfo MovementInformation { get; }
+	}
+}
