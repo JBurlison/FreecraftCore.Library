8745d056b27a6339748e9a3acc7b0e8199b0b582
HelloKitty <andrew.blakely@ymail.com>
2020-07-03 03:10:49 -0500

    Implemented CMSG_CANCEL_AURA packet

---
+++ src/FreecraftCore.Packet.Game/Packets/Spell/Aura/CMSG_CANCEL_AURA_Payload.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Packet payload structure for the <see cref="NetworkOperationCode"/> CMSG_CANCEL_AURA.
+	/// </summary>
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.CMSG_CANCEL_AURA)]
+	public sealed class CMSG_CANCEL_AURA_Payload : GamePacketPayload
+	{
+		/// <summary>
+		/// Represents the spell id to cancel the aura for.
+		/// </summary>
+		[WireMember(1)]
+		public int SpellId { get; internal set; }
+
+		public CMSG_CANCEL_AURA_Payload(int spellId)
+		{
+			if (spellId <= 0) throw new ArgumentOutOfRangeException(nameof(spellId));
+
+			SpellId = spellId;
+		}
+
+		/// <summary>
+		/// Default Serializer Ctor.
+		/// </summary>
+		internal CMSG_CANCEL_AURA_Payload()
+		{
+
+		}
+	}
+}
