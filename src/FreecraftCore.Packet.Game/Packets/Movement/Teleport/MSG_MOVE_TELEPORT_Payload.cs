b5e6ee1a6b6d6e39988fff4b0a17ac70fc5f780a
HelloKitty <andrew.blakely@ymail.com>
2020-07-05 01:41:01 -0500

    Implemented MSG_MOVE_TELEPORT packet

---
+++ src/FreecraftCore.Packet.Game/Packets/Movement/Teleport/MSG_MOVE_TELEPORT_Payload.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Packet payload structure for the <see cref="NetworkOperationCode"/> MSG_MOVE_TELEPORT.
+	/// </summary>
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.MSG_MOVE_TELEPORT)]
+	public sealed class MSG_MOVE_TELEPORT_Payload : GamePacketPayload, IPlayerMovementPayload<MovementInfo, MovementFlag, PackedGuid>
+	{
+		/// <inheritdoc />
+		[WireMember(1)]
+		public PackedGuid MovementGuid { get; internal set; }
+
+		/// <inheritdoc />
+		[WireMember(2)]
+		public MovementInfo MoveInfo { get; internal set; }
+
+		public MSG_MOVE_TELEPORT_Payload([NotNull] PackedGuid movementGuid, int sequenceCounter, [NotNull] MovementInfo moveInfo)
+		{
+			MovementGuid = movementGuid ?? throw new ArgumentNullException(nameof(movementGuid));
+			MoveInfo = moveInfo ?? throw new ArgumentNullException(nameof(moveInfo));
+		}
+		/// <summary>
+		/// Default Serializer Ctor.
+		/// </summary>
+		internal MSG_MOVE_TELEPORT_Payload()
+		{
+
+		}
+	}
+}
