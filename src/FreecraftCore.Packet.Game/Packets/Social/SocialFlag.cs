4cf9e1f3c2754b3d5e0513b2b66b1a493c93e39f
HelloKitty <andrew.blakely@ymail.com>
2020-07-06 02:04:17 -0500

    Added SocialFlag

---
+++ src/FreecraftCore.Packet.Game/Packets/Social/SocialFlag.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreecraftCore
+{
+	public enum SocialFlag
+	{
+		FRIEND = 0x01,
+
+		IGNORED = 0x02,
+
+		/// <summary>
+		/// Guessed
+		/// </summary>
+		MUTED = 0x04,
+
+		/// <summary>
+		///  Unknown - does not appear to be RaF
+		/// </summary>
+		UNK = 0x08,
+
+		SOCIAL_FLAG_ALL = FRIEND | IGNORED | MUTED
+	};
+}
