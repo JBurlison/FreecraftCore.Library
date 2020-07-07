79155d35abbf79b799da8f2bc6737e8fcd578907
HelloKitty <andrew.blakely@ymail.com>
2020-07-07 01:13:39 -0500

    Added QuestShareMessages

---
+++ src/FreecraftCore.API.Data/Core/Quest/Enums/QuestShareMessages.cs
@@ -0,0 +1,18 @@
+﻿namespace FreecraftCore
+{
+	public enum QuestShareMessages : byte
+	{
+		SHARING_QUEST = 0,
+		CANT_TAKE_QUEST = 1,
+		ACCEPT_QUEST = 2,
+		DECLINE_QUEST = 3,
+		BUSY = 4,
+		LOG_FULL = 5,
+		HAVE_QUEST = 6,
+		FINISH_QUEST = 7,
+		CANT_BE_SHARED_TODAY = 8,
+		SHARING_TIMER_EXPIRED = 9,
+		NOT_IN_PARTY = 10,
+		NOT_ELIGIBLE_TODAY = 11
+	};
+}
