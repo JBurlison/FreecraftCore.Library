800208a308455c1925a4f37c0db248fc81766a5e
HelloKitty <andrew.blakely@ymail.com>
2020-07-07 01:14:01 -0500

    Added QuestStatus

---
+++ src/FreecraftCore.API.Data/Core/Quest/Enums/QuestStatus.cs
@@ -0,0 +1,14 @@
+﻿namespace FreecraftCore
+{
+	public enum QuestStatus : byte
+	{
+		STATUS_NONE = 0,
+		STATUS_COMPLETE = 1,
+		//STATUS_UNAVAILABLE    = 2,
+		STATUS_INCOMPLETE = 3,
+		//STATUS_AVAILABLE      = 4,
+		STATUS_FAILED = 5,
+		STATUS_REWARDED = 6,        // Not used in DB
+		MAX_QUEST_STATUS
+	};
+}
