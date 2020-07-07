77fccb675499daba4cfde509c6eb2d146ee1939b
HelloKitty <andrew.blakely@ymail.com>
2020-07-07 01:14:10 -0500

    Added QuestTradeSkill

---
+++ src/FreecraftCore.API.Data/Core/Quest/Enums/QuestTradeSkill.cs
@@ -0,0 +1,21 @@
+﻿namespace FreecraftCore
+{
+	public enum QuestTradeSkill
+	{
+		NONE = 0,
+		ALCHEMY = 1,
+		BLACKSMITHING = 2,
+		COOKING = 3,
+		ENCHANTING = 4,
+		ENGINEERING = 5,
+		FIRSTAID = 6,
+		HERBALISM = 7,
+		LEATHERWORKING = 8,
+		POISONS = 9,
+		TAILORING = 10,
+		MINING = 11,
+		FISHING = 12,
+		SKINNING = 13,
+		JEWELCRAFTING = 14
+	};
+}
