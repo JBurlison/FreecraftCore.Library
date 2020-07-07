4eecf2eab3fb71ddb03e81e5b43ee126c3b645bc
HelloKitty <andrew.blakely@ymail.com>
2020-07-07 01:25:44 -0500

    Added QuestGreeting

---
+++ src/FreecraftCore.API.Data/Core/Quest/QuestGreeting.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+
+namespace FreecraftCore
+{
+	[WireDataContract]
+	public sealed class QuestGreeting
+	{
+		//Nullterminated string.
+		[Encoding(EncodingType.ASCII)]
+		[WireMember(2)]
+		public string GreetingText { get; internal set; }
+
+		//TODO: What is this??
+		[WireMember(3)]
+		public int GreetingDelay { get; internal set; }
+
+		/// <summary>
+		/// See: Emotes DBC.
+		/// </summary>
+		[WireMember(4)]
+		public int GreetingEmoteType { get; internal set; }
+
+		public QuestGreeting(string greetingText, int greetingDelay, int greetingEmoteType)
+		{
+			GreetingText = greetingText;
+			GreetingDelay = greetingDelay;
+			GreetingEmoteType = greetingEmoteType;
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		internal QuestGreeting()
+		{
+			
+		}
+	}
+}
