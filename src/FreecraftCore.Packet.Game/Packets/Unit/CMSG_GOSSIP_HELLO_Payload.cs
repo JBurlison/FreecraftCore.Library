bd26ce8c0cc9c37ea6c98d0473924a1f6ec1bf36
HelloKitty <andrew.blakely@ymail.com>
2020-07-07 00:22:22 -0500

    Implement CMSG_GOSSIP_HELLO packet

---
+++ src/FreecraftCore.Packet.Game/Packets/Unit/CMSG_GOSSIP_HELLO_Payload.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Packet payload structure for the <see cref="NetworkOperationCode"/> CMSG_GOSSIP_HELLO.
+	/// </summary>
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.CMSG_GOSSIP_HELLO)]
+	public sealed class CMSG_GOSSIP_HELLO_Payload : GamePacketPayload
+	{
+		[WireMember(1)]
+		public ObjectGuid Target { get; internal set; }
+
+		public CMSG_GOSSIP_HELLO_Payload([NotNull] ObjectGuid target)
+		{
+			Target = target ?? throw new ArgumentNullException(nameof(target));
+		}
+
+		/// <summary>
+		/// Default Serializer Ctor.
+		/// </summary>
+		internal CMSG_GOSSIP_HELLO_Payload()
+		{
+
+		}
+	}
+}
