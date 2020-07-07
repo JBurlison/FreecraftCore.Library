3e917c1690338b6bd0d6293f8af403cecbcfa867
HelloKitty <andrew.blakely@ymail.com>
2020-07-07 01:32:54 -0500

    Added SMSG_QUESTGIVER_QUEST_LIST packet

---
+++ src/FreecraftCore.Packet.Game/Packets/Quest/SMSG_QUESTGIVER_QUEST_LIST_Payload.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Packet payload structure for the <see cref="NetworkOperationCode"/> SMSG_QUESTGIVER_QUEST_LIST.
+	/// </summary>
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.SMSG_QUESTGIVER_QUEST_LIST)]
+	public sealed class SMSG_QUESTGIVER_QUEST_LIST_Payload : GamePacketPayload
+	{
+		/// <summary>
+		/// The GUID of the quest giver.
+		/// </summary>
+		[WireMember(1)]
+		public ObjectGuid QuestGiver { get; internal set; }
+
+		/// <summary>
+		/// The quests greeting type.
+		/// </summary>
+		[WireMember(2)]
+		public QuestGreeting Greeting { get; internal set; }
+
+		/// <summary>
+		/// The quest entries for the quest list.
+		/// </summary>
+		[SendSize(SendSizeAttribute.SizeType.Byte)]
+		[WireMember(3)]
+		internal QuestGossipEntry[] _Entries { get; set; }
+
+		[NotMapped]
+		public IEnumerable<QuestGossipEntry> Entries => _Entries;
+
+		public SMSG_QUESTGIVER_QUEST_LIST_Payload([NotNull] ObjectGuid questGiver, [NotNull] QuestGreeting greeting, [NotNull] QuestGossipEntry[] entries)
+		{
+			QuestGiver = questGiver ?? throw new ArgumentNullException(nameof(questGiver));
+			Greeting = greeting ?? throw new ArgumentNullException(nameof(greeting));
+			_Entries = entries ?? throw new ArgumentNullException(nameof(entries));
+		}
+
+		/// <summary>
+		/// Default Serializer Ctor.
+		/// </summary>
+		internal SMSG_QUESTGIVER_QUEST_LIST_Payload()
+		{
+
+		}
+	}
+}
