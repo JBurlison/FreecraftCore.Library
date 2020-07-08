af2b30af4951e424db37e44ad117d98076edda92
HelloKitty <andrew.blakely@ymail.com>
2020-07-07 23:35:02 -0500

    Implemented CMSG_QUESTGIVER_HELLO packet

---
+++ src/FreecraftCore.Packet.Game/Packets/Unit/CMSG_QUESTGIVER_HELLO_Payload.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Packet payload structure for the <see cref="NetworkOperationCode"/> CMSG_QUESTGIVER_HELLO.
+	/// </summary>
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.CMSG_QUESTGIVER_HELLO)]
+	public sealed class CMSG_QUESTGIVER_HELLO_Payload : GamePacketPayload
+	{
+		/// <summary>
+		/// The quest giver <see cref="ObjectGuid"/>.
+		/// </summary>
+		[WireMember(1)]
+		public ObjectGuid Target { get; internal set; }
+
+		public CMSG_QUESTGIVER_HELLO_Payload([NotNull] ObjectGuid target)
+		{
+			Target = target ?? throw new ArgumentNullException(nameof(target));
+		}
+
+		/// <summary>
+		/// Default Serializer Ctor.
+		/// </summary>
+		internal CMSG_QUESTGIVER_HELLO_Payload()
+		{
+
+		}
+	}
+}
