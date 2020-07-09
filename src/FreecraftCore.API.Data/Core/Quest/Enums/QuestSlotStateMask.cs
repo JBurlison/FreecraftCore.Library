0fab106f887b44782c6a65d237ed175be884ebaa
HelloKitty <andrew.blakely@ymail.com>
2020-07-09 04:57:44 -0500

    Added QuestSlotStateMask enum

---
+++ src/FreecraftCore.API.Data/Core/Quest/Enums/QuestSlotStateMask.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Sent in <see cref="EPlayerFields"/> Quest State.
+	/// </summary>
+	[Flags]
+	public enum QuestSlotStateMask
+	{
+		NONE = 0x0000,
+		COMPLETE = 0x0001,
+		FAIL = 0x0002
+	};
+}
