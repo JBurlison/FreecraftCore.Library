8cff705433c07c8ce0ec232b18891f34b14da33a
HelloKitty <andrew.blakely@ymail.com>
2020-07-08 23:37:31 -0500

    Implemented SMSG_GOSSIP_COMPLETE packet

---
+++ src/FreecraftCore.Packet.Game/Packets/Gossip/SMSG_GOSSIP_COMPLETE_Payload.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Packet payload structure for the <see cref="NetworkOperationCode"/> SMSG_GOSSIP_COMPLETE.
+	/// </summary>
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.SMSG_GOSSIP_COMPLETE)]
+	public sealed class SMSG_GOSSIP_COMPLETE_Payload : GamePacketPayload
+	{
+		//Just a command opcode. Contains no actual data.
+		/// <summary>
+		/// Default Serializer Ctor.
+		/// </summary>
+		public SMSG_GOSSIP_COMPLETE_Payload()
+		{
+
+		}
+	}
+}
