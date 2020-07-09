edee0005abe0a326bedc6bade76b7d2e5ae50ff3
HelloKitty <andrew.blakely@ymail.com>
2020-07-08 23:30:40 -0500

    Implemented CMSG_QUESTGIVER_ACCEPT_QUEST packet

---
+++ src/FreecraftCore.Packet.Game/Packets/Quest/CMSG_QUESTGIVER_ACCEPT_QUEST_Payload.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Packet payload structure for the <see cref="NetworkOperationCode"/> CMSG_QUESTGIVER_ACCEPT_QUEST.
+	/// </summary>
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.CMSG_QUESTGIVER_ACCEPT_QUEST)]
+	public sealed class CMSG_QUESTGIVER_ACCEPT_QUEST_Payload : GamePacketPayload
+	{
+		/// <summary>
+		/// The <see cref="ObjectGuid"/> of the quest giver.
+		/// </summary>
+		[WireMember(1)]
+		public ObjectGuid QuestGiver { get; internal set; }
+
+		/// <summary>
+		/// The id of the quest.
+		/// </summary>
+		[WireMember(2)]
+		public int QuestId { get; internal set; }
+
+		//TODO: TrinityCore reads this but does nothing with it.
+		[WireMember(3)]
+		internal int StartCheat { get; set; } =  0;
+
+		public CMSG_QUESTGIVER_ACCEPT_QUEST_Payload([NotNull] ObjectGuid questGiver, int questId)
+		{
+			QuestGiver = questGiver ?? throw new ArgumentNullException(nameof(questGiver));
+			QuestId = questId;
+		}
+
+		/// <summary>
+		/// Default Serializer Ctor.
+		/// </summary>
+		internal CMSG_QUESTGIVER_ACCEPT_QUEST_Payload()
+		{
+
+		}
+	}
+}
