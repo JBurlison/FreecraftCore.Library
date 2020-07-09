d0a8a41b3deb686271fa0ae95bb08e2fe75564fa
HelloKitty <andrew.blakely@ymail.com>
2020-07-09 04:57:37 -0500

    Added QuestSlotOffsets enum

---
+++ src/FreecraftCore.API.Data/Core/Quest/Enums/QuestSlotOffsets.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace FreecraftCore
+{
+	//TODO: Make quest consts
+	//#define MAX_QUEST_OFFSET 5
+
+	public enum QuestSlotOffsets
+	{
+		QUEST_ID_OFFSET = 0,
+		QUEST_STATE_OFFSET = 1,
+		QUEST_COUNTS_OFFSET = 2,
+		QUEST_TIME_OFFSET = 4
+	};
+}
