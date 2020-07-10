c52bd7b1cbc9d323e94e29a4815f0808ad4235d3
HelloKitty <andrew.blakely@ymail.com>
2020-07-09 21:12:34 -0500

    Implemented CMSG_QUESTGIVER_COMPLETE_QUEST packet

---
+++ src/FreecraftCore.Packet.Game/Packets/Quest/CMSG_QUESTGIVER_COMPLETE_QUEST_Payload.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Packet payload structure for the <see cref="NetworkOperationCode"/> CMSG_QUESTGIVER_COMPLETE_QUEST.
+	/// Represents just a REQUEST to complete a quest. It can be thought of conceptually
+	/// as a Try Complete. The server may redirect to Required Items page or Complete page
+	/// through afew different packets.
+	/// </summary>
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.CMSG_QUESTGIVER_COMPLETE_QUEST)]
+	public sealed class CMSG_QUESTGIVER_COMPLETE_QUEST_Payload : GamePacketPayload
+	{
+		/// <summary>
+		/// The <see cref="ObjectGuid"/> of the quest giver.
+		/// </summary>
+		[WireMember(1)]
+		public ObjectGuid QuestGiver { get; internal set; }
+
+		[WireMember(2)]
+		public int QuestId { get; internal set; }
+
+		public CMSG_QUESTGIVER_COMPLETE_QUEST_Payload([NotNull] ObjectGuid questGiver, int questId)
+		{
+			QuestGiver = questGiver ?? throw new ArgumentNullException(nameof(questGiver));
+			QuestId = questId;
+		}
+
+		/// <summary>
+		/// Default Serializer Ctor.
+		/// </summary>
+		internal CMSG_QUESTGIVER_COMPLETE_QUEST_Payload()
+		{
+
+		}
+	}
+}
