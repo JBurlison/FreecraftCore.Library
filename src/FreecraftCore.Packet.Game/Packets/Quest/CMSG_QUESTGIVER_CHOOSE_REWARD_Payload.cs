30b00335a2703a12f3f72bc250213ae7e9b89251
HelloKitty <andrew.blakely@ymail.com>
2020-07-11 13:43:36 -0500

    Added documentation to CMSG_QUESTGIVER_CHOOSE_REWARD_Payload

--- src/FreecraftCore.Packet.Game/Packets/Quest/CMSG_QUESTGIVER_CHOOSE_REWARD_Payload.cs
+++ src/FreecraftCore.Packet.Game/Packets/Quest/CMSG_QUESTGIVER_CHOOSE_REWARD_Payload.cs
@@ -8,17 +8,29 @@ namespace FreecraftCore
 {
 	/// <summary>
 	/// Packet payload structure for the <see cref="NetworkOperationCode"/> CMSG_QUESTGIVER_CHOOSE_REWARD.
+	/// Packet is used to turn-in or complete a quest. Even for quests that don't select a reward
+	/// this is actually sent. Probably with index 0 for the reward slot.
 	/// </summary>
 	[WireDataContract]
 	[GamePayloadOperationCode(NetworkOperationCode.CMSG_QUESTGIVER_CHOOSE_REWARD)]
 	public sealed class CMSG_QUESTGIVER_CHOOSE_REWARD_Payload : GamePacketPayload
 	{
+		/// <summary>
+		/// The <see cref="ObjectGuid"/> of the quest giver.
+		/// </summary>
 		[WireMember(1)]
 		public ObjectGuid QuestGiver { get; internal set; }
 
+		/// <summary>
+		/// The quest id.
+		/// </summary>
 		[WireMember(2)]
 		public int QuestId { get; internal set; }
 
+		/// <summary>
+		/// The reward slot to claim.
+		/// Should be 0 if none.
+		/// </summary>
 		[WireMember(3)]
 		public int RewardSlot { get; internal set; }
 
