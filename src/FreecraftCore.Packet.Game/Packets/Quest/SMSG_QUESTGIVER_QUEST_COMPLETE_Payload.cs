404d79696165afc162e635b568b4e2bee6f5af56
HelloKitty <andrew.blakely@ymail.com>
2020-07-11 22:03:34 -0500

    Implemented SMSG_QUESTGIVER_QUEST_COMPLETE packet

---
+++ src/FreecraftCore.Packet.Game/Packets/Quest/SMSG_QUESTGIVER_QUEST_COMPLETE_Payload.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Packet payload structure for the <see cref="NetworkOperationCode"/> SMSG_QUESTGIVER_QUEST_COMPLETE.
+	/// </summary>
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.SMSG_QUESTGIVER_QUEST_COMPLETE)]
+	public sealed class SMSG_QUESTGIVER_QUEST_COMPLETE_Payload : GamePacketPayload
+	{
+		[WireMember(1)]
+		public int QuestId { get; internal set; }
+
+		[WireMember(2)]
+		public int ExperienceRewarded { get; internal set; }
+
+		[WireMember(3)]
+		public int MoneyRewarded { get; internal set; }
+
+		//TODO: There is more stuff here, but not implementing it at this time. See Player::SendQuestReward
+
+		public SMSG_QUESTGIVER_QUEST_COMPLETE_Payload(int questId, int experienceRewarded, int moneyRewarded)
+		{
+			if (questId <= 0) throw new ArgumentOutOfRangeException(nameof(questId));
+
+			QuestId = questId;
+			ExperienceRewarded = experienceRewarded;
+			MoneyRewarded = moneyRewarded;
+		}
+
+		/// <summary>
+		/// Default Serializer Ctor.
+		/// </summary>
+		internal SMSG_QUESTGIVER_QUEST_COMPLETE_Payload()
+		{
+
+		}
+	}
+}
