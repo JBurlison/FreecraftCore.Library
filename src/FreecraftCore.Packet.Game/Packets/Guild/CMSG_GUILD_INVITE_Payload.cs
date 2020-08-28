910cf251004c6116fe1a5048f5a5d0636ea33da5
HelloKitty <andrew.blakely@ymail.com>
2020-08-28 18:59:19 -0500

    Implemented CMSG_GUILD_INVITE packet

---
+++ src/FreecraftCore.Packet.Game/Packets/Guild/CMSG_GUILD_INVITE_Payload.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Packet payload structure for the <see cref="NetworkOperationCode"/> CMSG_GUILD_INVITE.
+	/// </summary>
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.CMSG_GUILD_INVITE)]
+	public sealed class CMSG_GUILD_INVITE_Payload : GamePacketPayload
+	{
+		[Encoding(EncodingType.ASCII)]
+		[WireMember(1)]
+		public string PlayerToInviteName { get; internal set; }
+
+		public CMSG_GUILD_INVITE_Payload([NotNull] string playerToInviteName)
+		{
+			if (string.IsNullOrWhiteSpace(playerToInviteName)) throw new ArgumentException("Value cannot be null or whitespace.", nameof(playerToInviteName));
+
+			PlayerToInviteName = playerToInviteName;
+		}
+
+		/// <summary>
+		/// Default Serializer Ctor.
+		/// </summary>
+		internal CMSG_GUILD_INVITE_Payload()
+		{
+
+		}
+	}
+}
