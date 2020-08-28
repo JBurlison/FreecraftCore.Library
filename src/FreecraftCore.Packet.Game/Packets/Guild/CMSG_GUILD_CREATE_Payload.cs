aa31d74ff1bfe6eff01dc1ca16221fa4f6843491
HelloKitty <andrew.blakely@ymail.com>
2020-08-28 18:50:48 -0500

    Implemented unused CMSG_GUILD_CREATE packet

---
+++ src/FreecraftCore.Packet.Game/Packets/Guild/CMSG_GUILD_CREATE_Payload.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Unusued!
+	/// Packet payload structure for the <see cref="NetworkOperationCode"/> CMSG_GUILD_CREATE.
+	/// </summary>
+	[Obsolete("TrinityCore does not implement this packet.")]
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.CMSG_GUILD_CREATE)]
+	public sealed class CMSG_GUILD_CREATE_Payload : GamePacketPayload
+	{
+		[Encoding(EncodingType.ASCII)]
+		[WireMember(1)]
+		public string GuildName { get; internal set; }
+
+		public CMSG_GUILD_CREATE_Payload([NotNull] string guildName)
+		{
+			if (string.IsNullOrWhiteSpace(guildName)) throw new ArgumentException("Value cannot be null or whitespace.", nameof(guildName));
+
+			GuildName = guildName;
+		}
+
+		/// <summary>
+		/// Default Serializer Ctor.
+		/// </summary>
+		internal CMSG_GUILD_CREATE_Payload()
+		{
+
+		}
+	}
+}
