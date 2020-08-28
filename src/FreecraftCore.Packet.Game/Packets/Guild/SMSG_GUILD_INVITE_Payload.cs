22a18d946d6b11621a49dc640bd12a3689ad0f27
HelloKitty <andrew.blakely@ymail.com>
2020-08-28 18:38:00 -0500

    Implemented SMSG_GUILD_INVITE packet

---
+++ src/FreecraftCore.Packet.Game/Packets/Guild/SMSG_GUILD_INVITE_Payload.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Packet payload structure for the <see cref="NetworkOperationCode"/> SMSG_GUILD_INVITE.
+	/// </summary>
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.SMSG_GUILD_INVITE)]
+	public sealed class SMSG_GUILD_INVITE_Payload : GamePacketPayload
+	{
+		[Encoding(EncodingType.ASCII)]
+		[WireMember(1)]
+		public string InvitingPlayerName { get; internal set; }
+
+		[Encoding(EncodingType.ASCII)]
+		[WireMember(2)]
+		public string GuildName { get; internal set; }
+
+		public SMSG_GUILD_INVITE_Payload([NotNull] string invitingPlayerName, [NotNull] string guildName)
+		{
+			if (string.IsNullOrWhiteSpace(invitingPlayerName)) throw new ArgumentException("Value cannot be null or whitespace.", nameof(invitingPlayerName));
+			if (string.IsNullOrWhiteSpace(guildName)) throw new ArgumentException("Value cannot be null or whitespace.", nameof(guildName));
+
+			InvitingPlayerName = invitingPlayerName;
+			GuildName = guildName;
+		}
+
+		/// <summary>
+		/// Default Serializer Ctor.
+		/// </summary>
+		internal SMSG_GUILD_INVITE_Payload()
+		{
+
+		}
+	}
+}
