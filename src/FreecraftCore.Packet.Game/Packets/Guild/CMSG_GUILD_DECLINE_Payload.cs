3ea94af5b681cb654446b7c12c833524d926adec
HelloKitty <andrew.blakely@ymail.com>
2020-08-28 19:05:56 -0500

    Implemented CMSG_GUILD_DECLINE packet

---
+++ src/FreecraftCore.Packet.Game/Packets/Guild/CMSG_GUILD_DECLINE_Payload.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Packet payload structure for the <see cref="NetworkOperationCode"/> CMSG_GUILD_DECLINE.
+	/// </summary>
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.CMSG_GUILD_DECLINE)]
+	public sealed class CMSG_GUILD_DECLINE_Payload : GamePacketPayload
+	{
+		//Empty, just means it's declined.
+
+		/// <summary>
+		/// Default Serializer Ctor.
+		/// </summary>
+		public CMSG_GUILD_DECLINE_Payload()
+		{
+
+		}
+	}
+}
