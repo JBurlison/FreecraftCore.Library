8faf4c21593a7a5a6b3c167b57c8aced32782374
HelloKitty <andrew.blakely@ymail.com>
2020-08-28 19:02:00 -0500

    Implemented CMSG_GUILD_ACCEPT packet

---
+++ src/FreecraftCore.Packet.Game/Packets/Guild/CMSG_GUILD_ACCEPT_Payload.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Packet payload structure for the <see cref="NetworkOperationCode"/> CMSG_GUILD_ACCEPT.
+	/// </summary>
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.CMSG_GUILD_ACCEPT)]
+	public sealed class CMSG_GUILD_ACCEPT_Payload : GamePacketPayload
+	{
+		//Packet is EMPTY, just means accept.
+
+		/// <summary>
+		/// Default Serializer Ctor.
+		/// </summary>
+		public CMSG_GUILD_ACCEPT_Payload()
+		{
+
+		}
+	}
+}
