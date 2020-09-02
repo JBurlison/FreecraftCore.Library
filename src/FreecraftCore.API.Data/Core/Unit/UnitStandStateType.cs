9312d8e3be2988266731545b409f5c6a41eb2b26
HelloKitty <andrew.blakely@ymail.com>
2020-09-02 07:29:21 -0500

    Added UnitStandStateType

---
+++ src/FreecraftCore.API.Data/Core/Unit/UnitStandStateType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreecraftCore
+{
+	// byte value (UNIT_FIELD_BYTES_1, 0)
+	public enum UnitStandStateType
+	{
+		STATE_STAND = 0,
+		STATE_SIT = 1,
+		STATE_SIT_CHAIR = 2,
+		STATE_SLEEP = 3,
+		STATE_SIT_LOW_CHAIR = 4,
+		STATE_SIT_MEDIUM_CHAIR = 5,
+		STATE_SIT_HIGH_CHAIR = 6,
+		STATE_DEAD = 7,
+		STATE_KNEEL = 8,
+		STATE_SUBMERGED = 9
+	}
+}
