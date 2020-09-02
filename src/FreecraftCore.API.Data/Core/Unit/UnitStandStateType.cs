ba3e75e42ade2efc328ce1d238ffbb2f779ce791
HelloKitty <andrew.blakely@ymail.com>
2020-09-02 12:22:46 -0500

    Fixed UnitStandStateType

--- src/FreecraftCore.API.Data/Core/Unit/UnitStandStateType.cs
+++ src/FreecraftCore.API.Data/Core/Unit/UnitStandStateType.cs
@@ -7,15 +7,15 @@ namespace FreecraftCore
 	// byte value (UNIT_FIELD_BYTES_1, 0)
 	public enum UnitStandStateType
 	{
-		STATE_STAND = 0,
-		STATE_SIT = 1,
-		STATE_SIT_CHAIR = 2,
-		STATE_SLEEP = 3,
-		STATE_SIT_LOW_CHAIR = 4,
-		STATE_SIT_MEDIUM_CHAIR = 5,
-		STATE_SIT_HIGH_CHAIR = 6,
-		STATE_DEAD = 7,
-		STATE_KNEEL = 8,
-		STATE_SUBMERGED = 9
+		STAND = 0,
+		SIT = 1,
+		SIT_CHAIR = 2,
+		SLEEP = 3,
+		SIT_LOW_CHAIR = 4,
+		SIT_MEDIUM_CHAIR = 5,
+		SIT_HIGH_CHAIR = 6,
+		DEAD = 7,
+		KNEEL = 8,
+		SUBMERGED = 9
 	}
 }
