8af090df67b400817f209f2a604b73417f9ae614
HelloKitty <andrew.blakely@ymail.com>
2020-09-02 07:39:11 -0500

    Implemented SMSG_STANDSTATE_UPDATE packet

---
+++ src/FreecraftCore.Packet.Game/Packets/Social/SMSG_STANDSTATE_UPDATE_Payload.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Packet payload structure for the <see cref="NetworkOperationCode"/> SMSG_STANDSTATE_UPDATE.
+	/// </summary>
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.SMSG_STANDSTATE_UPDATE)]
+	public sealed class SMSG_STANDSTATE_UPDATE_Payload : GamePacketPayload
+	{
+		[WireMember(1)]
+		internal byte _State { get; set; }
+
+		public UnitStandStateType State => (UnitStandStateType)_State;
+
+		public SMSG_STANDSTATE_UPDATE_Payload(UnitStandStateType state)
+		{
+			_State = (byte) state;
+		}
+
+		/// <summary>
+		/// Default Serializer Ctor.
+		/// </summary>
+		internal SMSG_STANDSTATE_UPDATE_Payload()
+		{
+
+		}
+	}
+}
