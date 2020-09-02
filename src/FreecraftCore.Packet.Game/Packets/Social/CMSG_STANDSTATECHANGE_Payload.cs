6afab1c42ed8cf001a3e02945fcc77b2e9ba7d61
HelloKitty <andrew.blakely@ymail.com>
2020-09-02 07:34:56 -0500

    Added CMSG_STANDSTATECHANGE_Payload

---
+++ src/FreecraftCore.Packet.Game/Packets/Social/CMSG_STANDSTATECHANGE_Payload.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Packet payload structure for the <see cref="NetworkOperationCode"/> CMSG_STANDSTATECHANGE.
+	/// </summary>
+	[WireDataContract]
+	[GamePayloadOperationCode(NetworkOperationCode.CMSG_STANDSTATECHANGE)]
+	public sealed class CMSG_STANDSTATECHANGE_Payload : GamePacketPayload
+	{
+		[WireMember(1)]
+		public UnitStandStateType State { get; internal set; }
+
+		public CMSG_STANDSTATECHANGE_Payload(UnitStandStateType state)
+		{
+			if(!Enum.IsDefined(typeof(UnitStandStateType), state)) throw new InvalidEnumArgumentException(nameof(state), (int)state, typeof(UnitStandStateType));
+
+			State = state;
+		}
+
+		/// <summary>
+		/// Default Serializer Ctor.
+		/// </summary>
+		internal CMSG_STANDSTATECHANGE_Payload()
+		{
+
+		}
+	}
+}
